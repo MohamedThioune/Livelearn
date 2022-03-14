84936586e8a0153b218744055ace7a31d2df18e6
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-03-14 15:58:03 +0000

    Feedback removal

--- template-parts/modules/dashboard-menu-user.php
+++ template-parts/modules/dashboard-menu-user.php
@@ -61,7 +61,7 @@ $experts = get_user_meta($user->ID, 'expert');
                     echo "
                     <a href='/category-overview/?category=". $topic ."' class='d-flex'>
                         <div class='iconeElement'>
-                            <form action='dashboard/user/' method='POST'>
+                            <form action='/dashboard/user/' method='POST'>
                                 <input type='hidden' name='meta_value' value='". $topic . "' id=''>
                                 <input type='hidden' name='user_id' value='". $user->ID . "' id=''>
                                 <input type='hidden' name='meta_key' value='topic' id=''>
