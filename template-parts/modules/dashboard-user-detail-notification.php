7d13a92ff1c6258550e53dfc0c5612b89ee45e53
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-03-17 02:25:24 +0000

    push dashboard-detail-notification | final day ~ almost done

--- template-parts/modules/dashboard-user-detail-notification.php
+++ template-parts/modules/dashboard-user-detail-notification.php
@@ -54,12 +54,7 @@
                                                     echo '<input type="radio" id="star'.$index.'__" name="sales_rate_'.$index.'" value="" checked="checked" disabled="true"/>
                                                           <label class="ma_link" for="star'.$index.'__" title="text">'.$index.' stars</label>';
                                                 
-                                                for($in=$suit; $in >= 1; $in++)
-                                                    echo '<input type="radio" id="star'.$in.'__" name="sales_rate_'.$in.'" value=""  checked="checked" disabled="true"/>
-                                                          <label class="ma_link" for="star'.$in.'__" title="text">'.$in.' stars</label>';
-                                                
-
-                                                
+                                               
                                             echo "</div><br>";
                                             echo '<div class="mb-0" style="width: 100%;">'. $stopics_rates_comment[$i+2] . '</div>';
                                             $i = $i + 2;
