ad4bd96a6ca316e6b9fbdc12783d4321506918d4
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-03-21 18:20:28 +0000

    Correcting typos + Jquery issue

--- template-parts/modules/single-course-offline.php
+++ template-parts/modules/single-course-offline.php
@@ -900,7 +900,7 @@ $favoured = count(get_field('favorited', $post->ID));
 
 <!-- scritpt for modal -->
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
