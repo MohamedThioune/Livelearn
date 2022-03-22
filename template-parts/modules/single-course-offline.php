28065e5ba31c1eb7cf26fffe65f6cd58de892c41
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-03-22 20:21:43 +0000

    Patches Tues 22

--- template-parts/modules/single-course-offline.php
+++ template-parts/modules/single-course-offline.php
@@ -104,6 +104,9 @@ if(!$favoured)
         text-decoration: none !important;
         color: black !important;
     }
+    body{
+        padding-top: 0 !important;
+    }
 </style>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
