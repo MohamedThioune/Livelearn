fc0bd3957dfa723d8c4afcb653c841316f2bbfe6
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-03-22 20:42:23 +0000

    Patches Tues 22

--- template-parts/modules/dashboard-teacher-course-selection-add-video-3.php
+++ template-parts/modules/dashboard-teacher-course-selection-add-video-3.php
@@ -24,19 +24,19 @@
                     <div class="circleIndicator passEtape"></div>
                     <p class="textOpleidRight">Opleidingstype</p>
                 </a>
-                <a href="/dashboard/teacher/course-selection/?func=add-course&id=385&step=2" class="contentBlockCourse">
-                    <div class="circleIndicator  passEtape"></div>
+                <a href="/dashboard/teacher/course-selection/?func=add-video" class="contentBlockCourse">
+                    <div class="circleIndicator passEtape"></div>
                     <p class="textOpleidRight">Basis informatie</p>
                 </a>
-                <a href="/dashboard/teacher/course-selection/?func=add-course&id=<?php echo $_GET['id'];?>&step=2" class="contentBlockCourse">
+                <a href="/dashboard/teacher/course-selection/?func=add-video&id=<?php echo $_GET['id'];?>&step=2" class="contentBlockCourse">
                     <div class="circleIndicator passEtape"></div>
                     <p class="textOpleidRight">Uitgebreide beschrijving</p>
                 </a>
-                <a href="/dashboard/teacher/course-selection/?func=add-course&id=<?php echo $_GET['id'];?>&step=3" class="contentBlockCourse">
+                <a href="/dashboard/teacher/course-selection/?func=add-video&id=<?php echo $_GET['id'];?>&step=3" class="contentBlockCourse">
                     <div class="circleIndicator passEtape2"></div>
                     <p class="textOpleidRight ">Data en locaties</p>
                 </a>
-                <a href="/dashboard/teacher/course-selection/?func=add-course&id=<?php echo $_GET['id'];?>&step=4" class="contentBlockCourse">
+                <a href="/dashboard/teacher/course-selection/?func=add-video&id=<?php echo $_GET['id'];?>&step=4" class="contentBlockCourse">
                     <div class="circleIndicator"></div>
                     <p class="textOpleidRight">Details en onderwepren</p>
                 </a>
