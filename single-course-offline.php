0c1502337cf4f923804852b367ffea1d82f2cb2f
seydou <seydoukellel@gmail.com>
2022-03-24 15:13:57 +0000

    modife style

--- single-course-offline.php
+++ single-course-offline.php
@@ -71,14 +71,11 @@ $favoured = count(get_field('favorited', $post->ID));
     body{
         padding-top: 0px !important;
     }
-    a{
-        text-decoration: none !important;
-        color: black !important;
+    .dropdown-menu {
+        background: #043356;
+        color: white;
     }
 </style>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
 
 <!-- ---------------------------------------- Start modals ---------------------------------------------- -->
 <div class="modal fade" id="direct-contact" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
