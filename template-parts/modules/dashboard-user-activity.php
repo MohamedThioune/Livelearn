489d2f55e8566f1b666146b2202108b11f020554
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-03-24 12:17:36 +0000

    Patches Thu 24

--- template-parts/modules/dashboard-user-activity.php
+++ template-parts/modules/dashboard-user-activity.php
@@ -398,7 +398,7 @@ $todos = get_posts($args);
                         -->
                         <form action="" method="POST">
                             <input type="hidden" name="meta_value" value="<?=$course->ID;?>">
-                            <button type="submit" class="btn btnViewCours">
+                            <button type="submit" name="delete_favorite" class="btn btnViewCours">
                                 <img src="<?php echo get_stylesheet_directory_uri();?>/img/trashC.png" alt="">
                             </button>
                         </form>
