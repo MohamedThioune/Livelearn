6d4a074dc7f555304c82d5d1293917dea1cab399
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-04-20 14:13:59 +0000

    Wednesday response assessment

--- functions.php
+++ functions.php
@@ -216,6 +216,49 @@ function custom_post_type() {
 
     register_post_type( 'assessment', $assessment_args );
 
+    // - -- -
+
+    $response_assessment = array(
+        'name'                => _x( 'Response assessments', 'Response assessment', 'response_assessment' ),
+        'singular_name'       => _x( 'Response assessment', 'Response assessment', 'response_assessment' ),
+        'menu_name'           => __( 'Response assessments', 'response_assessment' ),
+        //'parent_item_colon'   => __( 'Parent Item:', 'fdfd_issue' ),
+        'all_items'           => __( 'All response_assessments', 'response_assessment' ),
+        'view_item'           => __( 'View response_assessment', 'view_response_assessment' ),
+        'add_new_item'        => __( 'New response_assessment', 'add_new_response_assessment' ),
+        'add_new'             => __( 'New response_assessment', 'text_domain' ),
+        'edit_item'           => __( 'Edit Item', 'text_domain' ),
+        'update_item'         => __( 'Update Item', 'text_domain' ),
+        'search_items'        => __( 'Search Item', 'text_domain' ),
+        'not_found'           => __( 'Not found', 'text_domain' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+    );
+
+    $response_assessment_args = array(
+        'label'               => __( 'response_assessment', 'text_domain' ),
+        'description'         => __( 'Post type for response_assessment', 'text_domain' ),
+        'labels'              => $response_assessment,
+        'supports'            => array('title', 'author', 'custom-fields', 'excerpt', 'thumbnail' ),
+        //'taxonomies'          => array('category', 'post_tag'),
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'menu_icon'           => '',
+        'can_export'          => true,
+        'rewrite'             => array('slug' => 'response_assessment'),
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'page',
+
+    );
+
+    register_post_type( 'response_assessment', $response_assessment_args );
+
 
     $assign = array(
         'name'                => _x( 'Assigns', 'Assigns', 'assign' ),
