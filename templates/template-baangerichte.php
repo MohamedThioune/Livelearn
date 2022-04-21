e4d9a1bc700be6a1b18c447bc5faa813c4001b31
seydou <seydoukellel@gmail.com>
2022-04-21 11:25:10 +0000

    menu

--- templates/template-baangerichte.php
+++ templates/template-baangerichte.php
@@ -2,8 +2,7 @@
 <body>
 <?php wp_head(); ?>
 <?php get_header(); ?>
-<div class="contentOne">
-</div>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/template.css" />
 <?php   
     
     $categories = array();
--- templates/template-video-course.php
+++ templates/template-video-course.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css">
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/custom.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/template.css" />
 
   <script src="https://kit.fontawesome.com/2def424b14.js" crossorigin="anonymous"></script>
   <title>Video Course</title>
