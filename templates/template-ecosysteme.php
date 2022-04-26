a0cae7faafdd519f7755e3dc10adbedbb32bd38a
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-04-26 01:32:20 +0000

    Monday 25 April - Apply changes from word

--- templates/template-ecosysteme.php
+++ templates/template-ecosysteme.php
@@ -16,10 +16,7 @@ $title_category = get_field('title_category', 'category_'. $topic) ? get_field('
 $descriptor_category = get_field('descriptor_category', 'category_'. $topic) ? get_field('descriptor_category', 'category_'. $topic) : 'Geen inhoud' ;
 $partners_category = get_field('partners_category', 'category_'. $topic);
 
-$image_category = get_field('image', 'category_'. $topic) ? get_field('image', 'category_'. $topic)['url'] : get_stylesheet_directory_uri() .'/ecosystemHeadImg.png' ;
-
-$banner_category = get_field('banner_category', 'category_'. $topic) ? get_field('banner_category', 'category_'. $topic)['url'] : get_stylesheet_directory_uri() .'/ecosystemHeadImg.png' ;
-
+$banner_category = get_field('image', 'category_'. $topic) ? get_field('image', 'category_'. $topic)['url'] : get_stylesheet_directory_uri() .'/ecosystemHeadImg.png' ;
 
 /*
 ** Leerpaden  owned *
@@ -241,12 +238,12 @@ foreach($global_blogs as $blog)
                         <div class="block-consultant">
                             <div class="block-initiative">
                                <div class="imgLivelearnLogo">
-                                   <img src="<?php echo get_stylesheet_directory_uri();?>/img/logo_right.png" alt="" >
+                                   <img src="<?php echo $partners_category[0]['image']; ?>" alt="" >
                                </div>
                             </div>
                             <div class="block-initiative block-initiative2">
                                <div class="imgLivelearnLogo">
-                                   <img src="<?php echo get_stylesheet_directory_uri();?>/img/Image49.png" alt="" >
+                                   <img src="<?php echo $partners_category[1]['image']; ?>" alt="" >
                                </div>                            
                             </div>
                         </div>
@@ -256,7 +253,7 @@ foreach($global_blogs as $blog)
             </div>
             <div class="col-lg-6">
                 <div class="img-head-ecosysteme">
-                    <img src="<?php echo $image_category; ?>" alt="">
+                    <img src="<?php echo $banner_category; ?>" alt="">
                 </div>
             </div>
 
