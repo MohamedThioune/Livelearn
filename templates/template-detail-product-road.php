7615cae02548e86ad709a292e5ec9921dd8f1fb8
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-05-08 18:29:02 +0000

    Sun 8 May - Review daily final complete push

--- templates/template-detail-product-road.php
+++ templates/template-detail-product-road.php
@@ -48,6 +48,8 @@ if(!$favoured)
 * *
 */
 
+$reviews = get_field('reviews', $leerpad->ID);
+
 
 ?>
 
@@ -145,6 +147,11 @@ if(!$favoured)
         <div class="container-fluid">
             <div class="overElement">
                 <div class="blockOneOver">
+                    <?php 
+                    if(isset($_GET["message"]))
+                        echo "<span class='alert alert-info'>" . $_GET['message'] . "</span><br><br>";
+                    ?>
+
                     <div class="titleBlock">
                         <div class="roundBlack" >
                             <img src="<?= $profile_picture; ?>" alt="company logo">
@@ -305,121 +312,77 @@ if(!$favoured)
                                 <li><a href="#tab3">Add Reviews</a></li>
                             </ul> <!-- END tabs-nav -->
                             <div id="tabs-content">
-                                <div id="tab2" class="tab-content">
-                                    <h2>Reviews</h2>
+                            <div id="tab2" class="tab-content">
+                                <?php
+                                if(!empty($reviews)){
+                                    foreach($reviews as $review){
+                                    ?>
                                     <div class="review-info-card">
                                         <div class="review-user-mini-profile">
                                             <div class="user-photo">
-                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/dan.jpg" alt="">
+                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/user.png" alt="">
                                             </div>
                                             <div class="user-name">
-                                                <p>Daniel</p>
+                                                <p><?= $review['name']; ?></p>
                                                 <div class="rating-element">
                                                     <div class="rating-stats">
                                                         <div id="rating-container-custom">
                                                             <ul class="list-show">
-                                                                <li class="selected"></li>
-                                                                <li class="selected"></li>
-                                                                <li class="selected"></li>
                                                                 <li class="disabled"></li>
                                                                 <li class="disabled"></li>
-                                                            </ul>
-                                                        </div>
-                                                        <p class="hours-element">18 hours ago</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <p class="reviewsText">Half the lanes don't work! But it's decent for the price. Waited over 30 minutes for our drinks!!!! But yes I recommend it. It's not great but it's OK I guess</p>
-
-                                    </div>
-                                    <div class="review-info-card">
-                                        <div class="review-user-mini-profile">
-                                            <div class="user-photo">
-                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/dan.jpg" alt="">
-                                            </div>
-                                            <div class="user-name">
-                                                <p>Daniel</p>
-                                                <div class="rating-element">
-                                                    <div class="rating-stats">
-                                                        <div id="rating-container">
-                                                            <ul class="list-show">
-                                                                <li class="selected"></li>
-                                                                <li class="selected"></li>
-                                                                <li class="selected"></li>
+                                                                <li class="disabled"></li>
                                                                 <li class="disabled"></li>
                                                                 <li class="disabled"></li>
                                                             </ul>
                                                         </div>
-                                                        <p class="hours-element">18 hours ago</p>
+                                                        <!-- <p class="hours-element">18 hours ago</p> -->
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <p class="reviewsText">Half the lanes don't work! But it's decent for the price. Waited over 30 minutes for our drinks!!!! But yes I recommend it. It's not great but it's OK I guess</p>
+                                        <p class="reviewsText"><?= $review['feedback']; ?></p>
 
                                     </div>
-                                    <div class="review-info-card">
-                                        <div class="review-user-mini-profile">
-                                            <div class="user-photo">
-                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/dan.jpg" alt="">
-                                            </div>
-                                            <div class="user-name">
-                                                <p>Daniel</p>
-                                                <div class="rating-element">
-                                                    <div class="rating-stats">
-                                                        <div id="rating-container">
-                                                            <ul class="list-show">
-                                                                <li class="selected"></li>
-                                                                <li class="selected"></li>
-                                                                <li class="selected"></li>
-                                                                <li class="disabled"></li>
-                                                                <li class="disabled"></li>
-                                                            </ul>
-                                                        </div>
-                                                        <p class="hours-element">18 hours ago</p>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                <?php
+                                    }
+                                }
+                                else 
+                                    echo "<h6>No reviews for this course ...</h6>";
+                                ?>
+                            </div>
+                            <div id="tab3" class="tab-content">
+                                <form action="../../dashboard/user/" method="POST">
+                                    <input type="hidden" name="course_id" value="<?= $post->ID; ?>">
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label for="name">Name</label>
+                                            <input type="text" name="fullname" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Name" required>
                                         </div>
-                                        <p class="reviewsText">Half the lanes don't work! But it's decent for the price. Waited over 30 minutes for our drinks!!!! But yes I recommend it. It's not great but it's OK I guess</p>
-
+                                        <div class="form- col-6">
+                                            <label for="exampleInputEmail1">Email address</label>
+                                            <input type="email" name="email_adress" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
+                                            <small id="exampleInputEmail1" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                        </div>
                                     </div>
-                                </div>
-                                <div id="tab3" class="tab-content">
-                                    <h2>Add review</h2>
-                                    <form>
-                                        <div class="row">
-                                            <div class="form-group col-6">
-                                                <label for="exampleInputEmail1">Name</label>
-                                                <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Name">
-                                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                                            </div>
-                                            <div class="form- col-6">
-                                                <label for="exampleInputEmail1">Email address</label>
-                                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                                            </div>
+                                    <!--<div class="form-group">
+                                        <label for="rating-container-custom">Rating</label>
+                                        <div id="rating-container-custom">
+                                            <ul class="list">
+                                                <li></li>
+                                                <li></li>
+                                                <li></li>
+                                                <li></li>
+                                                <li></li>
+                                            </ul>
                                         </div>
-                                        <div class="form-group">
-                                            <label for="rating-container-custom">Rating</label>
-                                            <div id="rating-container-custom">
-                                                <ul class="list">
-                                                    <li></li>
-                                                    <li></li>
-                                                    <li></li>
-                                                    <li></li>
-                                                    <li></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputPassword1">Feedback</label>
-                                            <textarea name="" id=""  rows="10"></textarea>
-                                        </div>
-                                        <button type="submit" class="btn btn-sendRating">Send</button>
-                                    </form>
-                                </div>
+                                    </div> -->
+                                    <div class="form-group">
+                                        <label for="">Feedback</label>
+                                        <textarea name="feedback_content" rows="10"></textarea>
+                                    </div>
+                                    <input type='submit' class='btn btn-sendRating' name='review_post' value='Send'>
+                                </form>
+                            </div>
                             </div> <!-- END tabs-content -->
                         </div> <!-- END tabs -->
                     </div>
