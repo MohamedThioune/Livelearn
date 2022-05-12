c438fd50e625de08d797f5fc23e049ac8ed79772
seydou <seydoukellel@gmail.com>
2022-05-12 08:47:12 +0000

    modife single and css

--- template-parts/modules/single-course-online.php
+++ template-parts/modules/single-course-online.php
@@ -105,7 +105,7 @@ $reviews = get_field('reviews', $post->ID);
         position: relative;
     }
     .liveOverBlock {
-        padding-top: 0px;
+        padding-top: 50px;
     }
 </style>
 
@@ -543,7 +543,7 @@ $reviews = get_field('reviews', $post->ID);
                                                 if(isset($lesson)) if($lesson == $sand) { ?>
                                                     <div class="d-flex contentListVidoeCourse">
                                                     <img class="playElement mr-3" style="width:22px;" src="<?php echo get_stylesheet_directory_uri() ?>/img/play.png" alt=""> <?php } ?>
-                                                <a href="?topic=<?php echo (int)$key; ?>&lesson=<?php echo (int)$sand; ?>" class="textChapitreCours"><?php echo ($value['course_lesson']['course_lesson_title']);?></a>
+                                                <a href="?topic=<?php echo (int)$key; ?>&lesson=<?php echo (int)$sand; ?>" class="textChapitreCours textChapitreCours2"><?php echo ($value['course_lesson']['course_lesson_title']);?></a>
                                                 </div>
 
 
