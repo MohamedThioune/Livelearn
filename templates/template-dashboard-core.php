2ea6c6c9e0d3ce77a32d2dc53b16e265f947e207
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-05-16 17:41:58 +0000

    push commit allocation draft js

--- templates/template-dashboard-core.php
+++ templates/template-dashboard-core.php
@@ -470,12 +470,12 @@ else if(isset($change_password)){
 }
 else if(isset($referee_employee)){
     //Already exists  
-    $allocution = get_field('allocation', $id_course);
+    $allocution = get_field('allocation', $course_id);
 
     //New subtopics
     $referees = $selected_members;
 
-    var_dump($referees);
+    //var_dump($referees);
 
     if(empty($referees))
         if(empty($allocution))
@@ -488,13 +488,14 @@ else if(isset($referee_employee)){
         else 
             $allocution = array_merge($allocution,$referees);
     
-    var_dump($allocution);
+    //var_dump($allocution);
+    
     //Adding new subtopics on course
-    update_field('allocation', $allocution, $id_course);
+    update_field('allocation', $allocution, $course_id);
 
     $message = get_permalink($id_course) . '/?message=Allocution successfully'; 
 
-    //header("Location: ". $message);
+    header("Location: ". $message);
 
 }
 
