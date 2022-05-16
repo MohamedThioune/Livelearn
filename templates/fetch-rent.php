28e15a795cedaac2a6723a8bd38c9a70a3c48d8c
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-05-16 19:15:02 +0000

    push commit allocation draft js

--- templates/fetch-rent.php
+++ templates/fetch-rent.php
@@ -19,6 +19,7 @@ foreach($users as $user) {
 if (isset($id_course))
 {
     echo "<input type='hidden' name='course_id' value='" . $id_course . "' />";
+    echo "<input type='hidden' name='path' value='dashboard' />";
     echo "<select multiple class='multipleSelect2' name='selected_members[]' >";
     if(!empty($users_company))
         foreach($users_company as $user){
