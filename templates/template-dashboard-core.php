7f8c233c4aeee3f4a3722b4c2f97b93878ca419e
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-05-18 11:05:24 +0000

    review unit test

--- templates/template-dashboard-core.php
+++ templates/template-dashboard-core.php
@@ -436,7 +436,7 @@ else if(isset($review_post)){
     $reviews = get_field('reviews', $course_id);
     $review = array();
     $review['user'] = get_user_by('ID',$user_id);
-    $review['rating'] = $rating;
+    $review['rate'] = $rating;
     $review['feedback'] = $feedback_content;
     if($review['user']){
         if(!$reviews)
