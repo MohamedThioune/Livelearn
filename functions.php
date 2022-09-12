838e318054e16135656235bdff8b694de5f0891d
Seydou Diallo <seydou.diallo@orange-sonatel.com>
2022-09-12 19:04:57 +0000

    modife big list

--- functions.php
+++ functions.php
@@ -567,3 +567,9 @@ add_filter( 'rest_authentication_errors', function( $result ) {
 });
 
 
+function remove_jquery_migrate_notice() {
+    $m= $GLOBALS['wp_scripts']->registered['jquery-migrate'];
+    $m->extra['before'][]='temp_jm_logconsole = window.console.log; window.console.log=null;';
+    $m->extra['after'][]='window.console.log=temp_jm_logconsole;';
+}
+add_action( 'init', 'remove_jquery_migrate_notice', 5 );
