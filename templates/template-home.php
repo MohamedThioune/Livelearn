234591bdf04387b53006fd35738061d18169d5eb
Seydou Diallo <seydou.diallo@orange-sonatel.com>
2022-09-26 19:04:27 +0000

    modife modal home page-

--- templates/template-home.php
+++ templates/template-home.php
@@ -10,7 +10,7 @@ require($page);
 ?>
 
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/template.css" />
-<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/mobiscroll.javascript.min.css" />
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/mobapiCity.js" />
 <script src="<?php echo get_stylesheet_directory_uri();?>/city.js"></script>
 
 <?php
@@ -1616,7 +1616,7 @@ $degrees=[
         </div>
     </div>
 </div>
-<script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-1b93190375e9ccc259df3a57c1abc0e64599724ae30d7ea4c6877eb615f89387.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri();?>/mobapiCity.js"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
