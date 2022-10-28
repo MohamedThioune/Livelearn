8be2a14d8dc33951213709183c601e9d576fab2f
Seydou Diallo <seydou.diallo@orange-sonatel.com>
2022-10-28 12:23:38 +0000

    new element

--- header_base.php
+++ header_base.php
@@ -62,6 +62,7 @@ $url = home_url( $wp->request );
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/custom.css" />
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/main.css" />
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/rating.css" />
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/header.css" />
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/chosen/1.4.2/chosen.min.css'>
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css'>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -75,7 +76,6 @@ $url = home_url( $wp->request );
         <meta property="og:description" content="Ontdek, ontwikkel en maak carriere">
         <meta property="og:image" content="<?php echo get_stylesheet_directory_uri() . '/img/logo_livelearn.png' ?>">
         <meta property="og:url" content="<?= $url ?>">
-
         <title><?php bloginfo('name'); ?></title>
         <?php wp_head(); ?>
     </head>
