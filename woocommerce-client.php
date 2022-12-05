636bc2259f4ac317f04f24e98f63126e21498571
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-12-05 22:48:21 +0000

    Woocommerce client wordpress

--- woocommerce-client.php
+++ woocommerce-client.php
@@ -8,15 +8,7 @@
 
 require_once __DIR__ . '/vendor/automattic/woocommerce/src/WooCommerce/Client.php';
 
-$woocommerce = new Client\Client(
-    'http://livelearn.nl',
-    'ck_f11f2d16fae904de303567e0fdd285c572c1d3f1',
-    'cs_3ba83db329ec85124b6f0c8cef5f647451c585fb',
-    [
-      'wp_api' => true,
-      'version' => 'wc/v3',
-    ]
-  );
+$woocommerce = new Client;
 
 print_r($woocommerce->get('orders'));
 ?>
