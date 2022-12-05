f711e70b12bea1e9ca8c8f63081f6911c2aa9f6a
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-12-05 16:42:02 +0000

    Skills note | Everywhere

--- templates/template-user-overview.php
+++ templates/template-user-overview.php
@@ -2035,15 +2035,22 @@
                             <div class="skills-side">
                                 <span class="text-dark h5 p-1 mt-2">My skills</span>
                                 
-                                <?php foreach($topics as $topic){ 
-                                        $name = (String)get_the_category_by_ID($topic); 
-                                        $rand = intval(rand(5, 100));   
+                                <?php foreach($topics as $value){ 
+                                        $topic = get_the_category_by_ID($value);
+                                        $note = 0;
+                                        if(!$topic)
+                                            continue;
+                                        if(!empty($skills_note))
+                                            foreach($skills_note as $skill)
+                                                if($skill['id'] == $value)
+                                                    $note = $skill['note'];
+                                        $name_topic = (String)$topic;    
                                     ?>
-                                    <div class="skillbars">
-                                        <label class="skillName"><?php echo $name_topic;  ?></label>
-                                        <div class="progress" data-fill="<?= $note ?>" >
+                                        <div class="skillbars">
+                                            <label class="skillName"><?php echo $name_topic;  ?></label>
+                                            <div class="progress" data-fill="<?= $note ?>" >
+                                            </div>
                                         </div>
-                                    </div>
                                 <?php } ?>
                             </div>    
                             <?php 
