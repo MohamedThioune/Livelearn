468c2ed24c8c900142807b99ba3d0ae897e9dd8f
Mohamed Thioune <mokhouthioune96@gmail.com>
2022-12-05 21:37:37 +0000

    company + missing informations

--- woocommerce-client.php
+++ woocommerce-client.php
@@ -1,8 +1,26 @@
-<?php /** Template Name: edit-databank */ ?>
+<?php /** Template Name: Client Wordpress */ ?>
 
 
 <?php wp_head(); ?>
 <?php get_header(); ?>
+
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Automattic\WooCommerce\Client;
+
+$woocommerce = new Client(
+    'http://livelearn.nl',
+    'ck_f11f2d16fae904de303567e0fdd285c572c1d3f1',
+    'cs_3ba83db329ec85124b6f0c8cef5f647451c585fb',
+    [
+      'version' => 'wc/v3',
+    ]
+  );
+
+$woocommerce = new Client($url, $consumer_key, $consumer_secret, $options);
+print_r($woocommerce->get('orders'));
+?>
 
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/template.css" />
 
