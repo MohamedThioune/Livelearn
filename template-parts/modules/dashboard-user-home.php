798f4a6760ced291ee28f9234ee0e112892cfb5c
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-02-12 16:22:08 +0000

    Redirection GET first login preferences

--- template-parts/modules/dashboard-user-home.php
+++ template-parts/modules/dashboard-user-home.php
@@ -177,6 +177,8 @@ if (isset($_POST["subtopics_first_login"])){
         }
     }
     update_field('is_first_login', true, 'user_'.get_current_user_id());
+    $message = "/dashboard/user/home/?message=Uw favoriete onderwerpen zijn succesvol opgeslagen !"; 
+    header("Location: ". $message);
 }
 
 $is_first_login = (get_field('is_first_login','user_' . get_current_user_id()));
