275184735edc74cd4e06c77657449416c1cdbd9a
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-02-12 18:09:56 +0000

    Single preferences final state

--- template-parts/modules/dashboard-user-home.php
+++ template-parts/modules/dashboard-user-home.php
@@ -243,7 +243,7 @@ if (!$is_first_login && get_current_user_id() !=0 )
                             </div>
                             <button type="button" class="btn btnNext" id="nextFunctiegericht">Next</button>
                         </div>
-                        <button type="button" class="btn btnSkipTopics" id="btnSkipTopics3">Skip</button>
+                        <button type="button" class="btn btnSkipTopics" id="btnSkipTopics2">Skip</button>
                     </div>
 
                     <div class="blockSkills">
