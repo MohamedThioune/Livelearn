2699f671556f01bbaf98978b21a762f36c6f0b4e
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-02-15 23:14:17 +0000

    Draft Overview dashboard user communities

--- template-parts/modules/dashboard-user-communities.php
+++ template-parts/modules/dashboard-user-communities.php
@@ -39,31 +39,38 @@ $other_communities = array();
 
                             $level = get_field('range', $community->ID);
 
-                            //Followers
-                            $max_follower = 0;
-                            $followers = get_field('follower_community', $community->ID);
-                            if(!empty($followers))
-                                $max_follower = count($followers);
-                            foreach ($followers as $key => $value)
-                            if($value->ID == $user_id){
-                                $bool = true;
-                                break;
-                            }
-
                             //Courses comin through custom field 
                             $courses = get_field('course_community', $community->ID);
                             $max_course = 0;
                             if(!empty($courses))
                                 $max_course = count($courses);
+
+                            //Followers
+                            $max_follower = 0;
+                            $followers = get_field('follower_community', $community->ID);
+                            if(!empty($followers))
+                                $max_follower = count($followers);
+
+                            foreach ($followers as $key => $value)
+                                if($value->ID == $user_id){
+                                    $bool = true;
+                                    break;
+                                }
+
+                            if($bool)
+                                array_push($your_communities, $community);
+                            else
+                                array_push($other_communities, $community);
+                            
                         ?>
                             <div class="card-communities">
                                 <div class="head-card-communities">
-                                    <img class="" src="<?= $company_image; ?>" alt="">
+                                    <img class="" src="<?= $community_image; ?>" alt="">
                                 </div>
                                 <div class="body-card-community">
                                     <div class="block-img-title d-flex align-items-center">
                                         <div class="imgCommunity">
-                                            <img class="" src="<?= $community_image; ?>" alt="">
+                                            <img class="" src="<?= $company_image; ?>" alt="">
                                         </div>
                                         <div>
                                             <p class="name-community"><?= $company->post_title; ?></p>
@@ -73,7 +80,7 @@ $other_communities = array();
                                     <div class="d-flex justify-content-center group-detail">
                                         <div>
                                             <p class="number-element"><?= $max_course ; ?></p>
-                                            <p class="description">Course</p>
+                                            <p class="description">Post</p>
                                         </div>
                                         <div class="block-detail">
                                             <p class="number-element"><?= $max_follower; ?></p>
@@ -102,134 +109,136 @@ $other_communities = array();
                     </div>
                     <div class="tab">
                         <div class="group-card-communities d-flex flex-wrap">
+                        <?php
+
+                        foreach($your_communities as $community){
+                            $company = get_field('company_author', $community->ID)[0];
+                            $company_image = (get_field('company_logo', $company->ID)) ? get_field('company_logo', $company->ID) : get_stylesheet_directory_uri() . '/img/business-and-trade.png';
+                            $community_image = get_field('image_community', $community->ID) ?: $company_image;
+
+                            $level = get_field('range', $community->ID);
+
+                            //Courses comin through custom field 
+                            $courses = get_field('course_community', $community->ID);
+                            $max_course = 0;
+                            if(!empty($courses))
+                                $max_course = count($courses);
+
+                            //Followers
+                            $max_follower = 0;
+                            $followers = get_field('follower_community', $community->ID);
+                            if(!empty($followers))
+                                $max_follower = count($followers);
+                        ?>
                             <div class="card-communities">
                                 <div class="head-card-communities">
-                                    <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-bg-11.png" alt="">
+                                    <img class="" src="<?= $community_image; ?>" alt="">
                                 </div>
                                 <div class="body-card-community">
                                     <div class="block-img-title d-flex align-items-center">
                                         <div class="imgCommunity">
-                                            <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-2.png" alt="">
+                                            <img class="" src="<?= $company_image; ?>" alt="">
                                         </div>
                                         <div>
-                                            <p class="name-community">Graphic Design</p>
-                                            <p class="statut-community">Public Groups</p>
+                                            <p class="name-community"><?= $company->post_title; ?></p>
+                                            <p class="statut-community">Private Groups</p>
                                         </div>
                                     </div>
                                     <div class="d-flex justify-content-center group-detail">
                                         <div>
-                                            <p class="number-element">245</p>
+                                            <p class="number-element"><?= $max_course ; ?></p>
                                             <p class="description">Post</p>
                                         </div>
                                         <div class="block-detail">
-                                            <p class="number-element">4245</p>
+                                            <p class="number-element"><?= $max_follower; ?></p>
                                             <p class="description">Members</p>
                                         </div>
                                         <div>
-                                            <p class="number-element">2010</p>
+                                            <p class="number-element">0000</p>
                                             <p class="description">Since</p>
                                         </div>
                                     </div>
-                                    <a href="" class="btn btn-join-group">Join Group</a>
+                                    <?php
+                                    if(!$bool)
+                                        echo "<form action='/dashboard/user/' method='POST'>
+                                                    <input type='hidden' name='community_id' value='" . $community->ID . "' >
+                                                    <input type='submit' class='btn btn-join-group' name='follow_community' value='Join Group' >
+                                              </form>";
+                                    else
+                                        // echo " <button type='button' class='btn btn-join-group' disabled>Join Group</button>";
+                                    ?>
                                 </div>
                             </div>
-                            <div class="card-communities">
-                                <div class="head-card-communities">
-                                    <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-bg-2.png" alt="">
-                                </div>
-                                <div class="body-card-community">
-                                    <div class="block-img-title d-flex align-items-center">
-                                        <div class="imgCommunity">
-                                            <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-2.png" alt="">
-                                        </div>
-                                        <div>
-                                            <p class="name-community">Marketing</p>
-                                            <p class="statut-community">Public Groups</p>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex justify-content-center group-detail">
-                                        <div>
-                                            <p class="number-element">245</p>
-                                            <p class="description">Post</p>
-                                        </div>
-                                        <div class="block-detail">
-                                            <p class="number-element">4245</p>
-                                            <p class="description">Members</p>
-                                        </div>
-                                        <div>
-                                            <p class="number-element">2010</p>
-                                            <p class="description">Since</p>
-                                        </div>
-                                    </div>
-                                    <a href="" class="btn btn-join-group">Join Group</a>
-                                </div>
-                            </div>
+                        <?php
+                        }
+                        ?>
                         </div>
                     </div>
                     <div class="tab">
                         <div class="group-card-communities d-flex flex-wrap">
+                        <?php
+
+                        foreach($other_communities as $community){
+                            $company = get_field('company_author', $community->ID)[0];
+                            $company_image = (get_field('company_logo', $company->ID)) ? get_field('company_logo', $company->ID) : get_stylesheet_directory_uri() . '/img/business-and-trade.png';
+                            $community_image = get_field('image_community', $community->ID) ?: $company_image;
+
+                            $level = get_field('range', $community->ID);
+
+                            //Courses comin through custom field 
+                            $courses = get_field('course_community', $community->ID);
+                            $max_course = 0;
+                            if(!empty($courses))
+                                $max_course = count($courses);
+
+                            //Followers
+                            $max_follower = 0;
+                            $followers = get_field('follower_community', $community->ID);
+                            if(!empty($followers))
+                                $max_follower = count($followers);
+                        ?>
                             <div class="card-communities">
                                 <div class="head-card-communities">
-                                    <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-bg-2.png" alt="">
+                                    <img class="" src="<?= $community_image; ?>" alt="">
                                 </div>
                                 <div class="body-card-community">
                                     <div class="block-img-title d-flex align-items-center">
                                         <div class="imgCommunity">
-                                            <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-2.png" alt="">
+                                            <img class="" src="<?= $company_image; ?>" alt="">
                                         </div>
                                         <div>
-                                            <p class="name-community">Marketing</p>
-                                            <p class="statut-community">Public Groups</p>
+                                            <p class="name-community"><?= $company->post_title; ?></p>
+                                            <p class="statut-community">Private Groups</p>
                                         </div>
                                     </div>
                                     <div class="d-flex justify-content-center group-detail">
                                         <div>
-                                            <p class="number-element">245</p>
+                                            <p class="number-element"><?= $max_course ; ?></p>
                                             <p class="description">Post</p>
                                         </div>
                                         <div class="block-detail">
-                                            <p class="number-element">4245</p>
+                                            <p class="number-element"><?= $max_follower; ?></p>
                                             <p class="description">Members</p>
                                         </div>
                                         <div>
-                                            <p class="number-element">2010</p>
+                                            <p class="number-element">0000</p>
                                             <p class="description">Since</p>
                                         </div>
                                     </div>
-                                    <a href="" class="btn btn-join-group">Join Group</a>
+                                    <?php
+                                    if(!$bool)
+                                        echo "<form action='/dashboard/user/' method='POST'>
+                                                    <input type='hidden' name='community_id' value='" . $community->ID . "' >
+                                                    <input type='submit' class='btn btn-join-group' name='follow_community' value='Join Group' >
+                                              </form>";
+                                    else
+                                        // echo " <button type='button' class='btn btn-join-group' disabled>Join Group</button>";
+                                    ?>
                                 </div>
                             </div>
-                            <div class="card-communities">
-                                <div class="head-card-communities">
-                                    <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-bg-3.png" alt="">
-                                </div>
-                                <div class="body-card-community">
-                                    <div class="block-img-title d-flex align-items-center">
-                                        <div class="imgCommunity">
-                                            <img class="" src="<?php echo get_stylesheet_directory_uri();?>/img/groups-2.png" alt="">
-                                        </div>
-                                        <div>
-                                            <p class="name-community">Developer</p>
-                                            <p class="statut-community">Public Groups</p>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex justify-content-center group-detail">
-                                        <div>
-                                            <p class="number-element">245</p>
-                                            <p class="description">Post</p>
-                                        </div>
-                                        <div class="block-detail">
-                                            <p class="number-element">4245</p>
-                                            <p class="description">Members</p>
-                                        </div>
-                                        <div>
-                                            <p class="number-element">2010</p>
-                                            <p class="description">Since</p>
-                                        </div>
-                                    </div>
-                                    <a href="" class="btn btn-join-group">Join Group</a>
-                                </div>
-                            </div>
+                        <?php
+                        }
+                        ?>
                         </div>
                     </div>
 
