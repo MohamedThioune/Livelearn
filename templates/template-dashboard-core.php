ecd0a0dd401e6fb90f80dd3864e216447e1b6474
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-02-20 03:51:48 +0000

    Community detail - communities

--- templates/template-dashboard-core.php
+++ templates/template-dashboard-core.php
@@ -900,7 +900,7 @@ else if(isset($follow_community)){
     update_field('follower_community', $followers, $community_id);
 
     $path = "/dashboard/user/communities/?mu=" . $community_id . "&message=Successfully subscribed to this community !";
-    header("Location: ". $path);
+    // header("Location: ". $path);
 }
 
 else if(isset($unfollow_community)){
