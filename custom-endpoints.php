1115a7012f239aa4642664d937687ccd54d55ea7
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-02-21 16:34:46 +0000

    API community

--- custom-endpoints.php
+++ custom-endpoints.php
@@ -1361,14 +1361,13 @@ function filter_course(WP_REST_Request $request)
 
         array_push($community_courses, $demand_course);
       }
-      $community->courses = $community_courses;
 
       $demand_community = (object)[
         'title' => $community->post_title,
         'description' => $community->post_excerpt,
         'picture' => $community->image,
         'created_at' => $community->post_date,
-        'courses' => $community->courses
+        'courses' => $community_courses
         
       ];
       array_push($communities, $demand_community);
