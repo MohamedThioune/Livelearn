08d0262c093539a7aff71ee6d54d60d59c389539
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-03-03 15:07:44 +0000

    Youtube video playlist --course

--- templates/youtube-video-course-playlist.php
+++ templates/youtube-video-course-playlist.php
@@ -25,7 +25,7 @@ $table = $wpdb->prefix . 'databank';
   <h1>Youtube data API V3 - Playlist </h1>
   <?php
   /** Template Name: Youtube Video V3 Playlist */ 
-  $api_key = "AIzaSyDesrtvddE6l7tfbsPB3CTexWtqLwgNBK8";
+  $api_key = "AIzaSyB0J1q8-LdT0994UBb6Q35Ff5ObY-Kqi_0";
   $maxResults = 45;
 
   $users = get_users();
