39494fce00b1f21605206393ef16abdf93b36374
pro <pro@mbp-de-pro.home>
2023-03-15 15:42:01 +0000

    new activity

--- template-parts/modules/dashboard-user-activity-2.php
+++ template-parts/modules/dashboard-user-activity-2.php
@@ -130,6 +130,242 @@
                                 </table>
                             </div>
                         </div>
+                        <div class="blockItemCourse notificationCourseCard">
+                            <div class="d-flex align-items-center justify-content-between head-blockItemCourse">
+                                <p class="title">Notifications</p>
+                                <a href="" class="d-flex align-items-center">
+                                    <p class="seeAllText">See All</p>
+                                    <img src="<?php echo get_stylesheet_directory_uri();?>/img/seeAllIcon.png" class="" alt="">
+                                </a>
+                            </div>
+                            <div class="card-notification">
+                                <div class="notificationBlock">
+                                    <div class="d-flex">
+                                        <div class="blockImgCard">
+                                            <img src="<?php echo get_stylesheet_directory_uri();?>/img/Daniel-van-der.png" class="" alt="">
+                                        </div>
+                                        <div>
+                                            <p class="name">Daniel</p>
+                                            <p class="time">1 hours ago</p>
+                                        </div>
+                                    </div>
+                                    <p class="descriptionNotification">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                </div>
+                                <div class="notificationBlock">
+                                    <div class="d-flex">
+                                        <div class="blockImgCard">
+                                            <img src="<?php echo get_stylesheet_directory_uri();?>/img/Daniel-van-der.png" class="" alt="">
+                                        </div>
+                                        <div>
+                                            <p class="name">Daniel</p>
+                                            <p class="time">1 hours ago</p>
+                                        </div>
+                                    </div>
+                                    <p class="descriptionNotification">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                </div>
+                                <div class="notificationBlock">
+                                    <div class="d-flex">
+                                        <div class="blockImgCard">
+                                            <img src="<?php echo get_stylesheet_directory_uri();?>/img/Daniel-van-der.png" class="" alt="">
+                                        </div>
+                                        <div>
+                                            <p class="name">Daniel</p>
+                                            <p class="time">1 hours ago</p>
+                                        </div>
+                                    </div>
+                                    <p class="descriptionNotification">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="data-analitycs-block">
+                            <div class="d-flex align-items-center justify-content-between head-blockItemCourse">
+                                <p class="title">Data and analytics</p>
+                                <a href="" class="d-flex align-items-center">
+                                    <p class="seeAllText">See All</p>
+                                    <img src="<?php echo get_stylesheet_directory_uri();?>/img/seeAllIcon.png" class="" alt="">
+                                </a>
+                            </div>
+                            <div class="content-data-analytics d-flex flex-wrap align-items-center justify-content-between">
+                                <div class="analytics-block">
+                                    <div class="first-content-analitycs text-left">
+                                        <p class="title-block">Analytics</p>
+                                        <p class="spent-text"> <b>22 K€</b> spent since the beginning of the adventure</p>
+                                        <div class="detail-analytic">
+                                            <div class="sub-detail">
+                                                <p class="number-sub-detail">29</p>
+                                                <p class="text-sub-detail">Profil view</p>
+                                            </div>
+                                            <div class="sub-detail">
+                                                <p class="number-sub-detail">18</p>
+                                                <p class="text-sub-detail">Course Done</p>
+                                            </div>
+                                            <div class="sub-detail">
+                                                <p class="number-sub-detail">2</p>
+                                                <p class="text-sub-detail">Profil view</p>
+                                            </div>
+                                            <div class="sub-detail">
+                                                <p class="number-sub-detail">29</p>
+                                                <p class="text-sub-detail">Spent / Day</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="second-content-analitycs">
+                                        <img src="<?php echo get_stylesheet_directory_uri();?>/img/analytics-detail.png" class="" alt="">
+                                    </div>
+                                </div>
+                                <div class="other-analytics">
+                                    <div class="d-flex justify-content-center">
+                                        <div class="detail-other-analytics">
+                                            <p class="number-other-detail">22</p>
+                                            <p class="text-other-detail">Articles</p>
+                                        </div>
+                                        <hr class="hrFirst">
+                                        <div class="detail-other-analytics">
+                                            <p class="number-other-detail">35</p>
+                                            <p class="text-other-detail">E-learning Cours</p>
+                                        </div>
+                                    </div>
+                                    <hr class="hrSecond">
+                                    <div class="d-flex justify-content-center">
+                                        <div class="detail-other-analytics">
+                                            <p class="number-other-detail">5</p>
+                                            <p class="text-other-detail">Podcast</p>
+                                        </div>
+                                        <hr class="hrFirst">
+                                        <div class="detail-other-analytics">
+                                            <p class="number-other-detail">15</p>
+                                            <p class="text-other-detail">Videos Cours</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="blockYourCertificates">
+                            <div class="d-flex align-items-center justify-content-between head-blockItemCourse">
+                                <p class="title">Your certificates</p>
+                                <a href="" class="d-flex align-items-center">
+                                    <p class="seeAllText">See All</p>
+                                    <img src="<?php echo get_stylesheet_directory_uri();?>/img/seeAllIcon.png" class="" alt="">
+                                </a>
+                            </div>
+                            <div class="card-all-certificat">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col courseTitle">Certificat No.</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Upload Date</th>
+                                        <th>Certificate</th>
+                                        <th>Controls</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <p class="text-center numberCertificat">1</p>
+                                        </td>
+                                        <td>
+                                            <p class="name-element">WordPress Certificate</p>
+                                        </td>
+                                        <td class="">
+                                            <p class="name-element">6 April 2019</p>
+                                        </td>
+                                        <td>
+                                            <a href="">View</a>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-trash">
+                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/clarity_trash-line.png" class="" alt="">
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p class="text-center numberCertificat">1</p>
+                                        </td>
+                                        <td>
+                                            <p class="name-element">WordPress Certificate</p>
+                                        </td>
+                                        <td class="">
+                                            <p class="name-element">6 April 2019</p>
+                                        </td>
+                                        <td>
+                                            <a href="">View</a>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-trash">
+                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/clarity_trash-line.png" class="" alt="">
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p class="text-center numberCertificat">1</p>
+                                        </td>
+                                        <td>
+                                            <p class="name-element">WordPress Certificate</p>
+                                        </td>
+                                        <td class="">
+                                            <p class="name-element">6 April 2019</p>
+                                        </td>
+                                        <td>
+                                            <a href="">View</a>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-trash">
+                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/clarity_trash-line.png" class="" alt="">
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="blockYourCertificates">
+                            <div class="d-flex align-items-center justify-content-between head-blockItemCourse">
+                                <p class="title">Assessments</p>
+                                <a href="" class="d-flex align-items-center">
+                                    <p class="seeAllText">See All</p>
+                                    <img src="<?php echo get_stylesheet_directory_uri();?>/img/seeAllIcon.png" class="" alt="">
+                                </a>
+                            </div>
+                            <div class="card-all-certificat">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col courseTitle">Questionnaire</th>
+                                        <th scope="col">Upload Date</th>
+                                        <th>Score</th>
+                                        <th>Manage</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <p class="text-center numberCertificat">WordPress Certificate</p>
+                                        </td>
+                                        <td>
+                                            <p class="name-element">6 April 2019</p>
+                                        </td>
+                                        <td class="">
+                                            <p class="name-element">18 %</p>
+                                        </td>
+                                        <td class="d-flex align-items-center justify-content-center">
+                                            <a href="">
+                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/clarity_eye-line.png" class="" alt="">
+                                            </a>
+                                            <button class="btn btn-trash">
+                                                <img src="<?php echo get_stylesheet_directory_uri();?>/img/clarity_trash-line.png" class="" alt="">
+                                            </button>
+                                        </td>
+                                    </tr>
+
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                     <div class="tab">
 z
