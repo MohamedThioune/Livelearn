aaddd44f8dd7edb1dff9909a9469db796e946aa9
Megamind <ibouddha1.if@gmail.com>
2023-03-15 19:29:41 +0000

    try catch

--- templates/template-plugin.php
+++ templates/template-plugin.php
@@ -237,7 +237,7 @@ function RandomString(){
         try{
           // var_dump($data);
           $wpdb->insert($table,$data);
-          // echo $wpdb->last_error." ".$url;
+          echo $key;
           $id_post = $wpdb->insert_id;
         }catch(Exception $e) {
           echo $e->getMessage();
