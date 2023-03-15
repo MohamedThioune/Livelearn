449b390fd2f311d9216132e495a9e77a1dac2c44
Megamind <ibouddha1.if@gmail.com>
2023-03-15 18:20:20 +0000

    without JS

--- templates/databank.php
+++ templates/databank.php
@@ -47,7 +47,7 @@ $websites = ['smartwp','DeZZP','fmn','duurzaamgebouwd','adformatie','morethandri
                    <p class="JouwOpleid"> <!-- Alle opleidingen --> <strong>Load From</strong> : &nbsp;
                        <a href="/youtube-v3-playlist" target="_blank"  class="JouwOpleid youtubeCourse"><img src="<?= get_stylesheet_directory_uri(); ?>/img/youtube.png" alt="youtube image"></a>
                        &nbsp;&nbsp;<a href="/xml-parse" target="_blank"  class="JouwOpleid youtubeCourse" style="border: #FF802B solid;"><img style="width: 35px;" width="15" src="<?= get_stylesheet_directory_uri(); ?>/img/xml-orange.jpg" alt="xml image"></a>
-                       <a id="bouddha" class="JouwOpleid youtubeCourse" style="border: #FF802B solid;"><img style="width: 35px;" width="15" src="<?= get_stylesheet_directory_uri(); ?>/img/article.jpg" alt="artikel" style="cursor:grab;"></a>
+                       <a id="bouddha" href="/artikels" class="JouwOpleid youtubeCourse" style="border: #FF802B solid;"><img style="width: 35px;" width="15" src="<?= get_stylesheet_directory_uri(); ?>/img/article.jpg" alt="artikel" style="cursor:grab;"></a>
                        &nbsp;&nbsp;<button id="subtopics" class="JouwOpleid youtubeCourse" style="border: #FF802B solid;" ><img style="width: 35px;" width="15" src="<?= get_stylesheet_directory_uri(); ?>/img/artikel.jpg" alt="load subtopics"></button>
                        
                     <div class="col-md-3">
@@ -216,30 +216,30 @@ $websites = ['smartwp','DeZZP','fmn','duurzaamgebouwd','adformatie','morethandri
     //     }
     // }
 
-    $('#bouddha').click((e)=>{
-        $('#select_field').hide(true,2000);
-        $('#loader').attr('hidden',false);
-        $.ajax({
-            url:'/artikels', 
-            type:'POST',
-            datatype:'json',
-            cache:false,
-            beforeSend:function(){
-            },
-            success:function(){
-                location.reload();
-                // window.location.href = "/livelearn/artikels";
-            },
-            complete:function(){
-                $('#select_field').hide(false,2000);
-                $('#loader').attr('hidden',true);
-                // window.location.href = "/livelearn/artikels";
-            },
-            error:function(error){
-                console.log('error: '+error);
-            }
-        }); 
-    });
+    // $('#bouddha').click((e)=>{
+    //     $('#select_field').hide(true,2000);
+    //     $('#loader').attr('hidden',false);
+    //     $.ajax({
+    //         url:'/artikels', 
+    //         type:'POST',
+    //         datatype:'json',
+    //         cache:false,
+    //         beforeSend:function(){
+    //         },
+    //         success:function(){
+    //             location.reload();
+    //             // window.location.href = "/livelearn/artikels";
+    //         },
+    //         complete:function(){
+    //             $('#select_field').hide(false,2000);
+    //             $('#loader').attr('hidden',true);
+    //             // window.location.href = "/livelearn/artikels";
+    //         },
+    //         error:function(error){
+    //             console.log('error: '+error);
+    //         }
+    //     }); 
+    // });
 
     $('#select_field').change((e)=>
     {
