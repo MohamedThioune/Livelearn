af41bc4a0b5cc04291fbeebc25359a6bb7c21489
pro <pro@mbp-de-pro.home>
2023-04-03 14:22:22 +0000

    new page front

---
+++ template-parts/modules/dashboard-user-checkout-offline.php
@@ -0,0 +1,185 @@
+<html lang="en">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet"/>
+
+
+<body>
+<div class="content-checkout-of">
+    <div class="head-checkout-of d-flex">
+        <div class="date-block">
+            <div class="moth">
+                <p>Jun</p>
+            </div>
+            <p class="date-time">01</p>
+        </div>
+        <div>
+            <p class="title-course-chechout-of">Tutorial on How to use Figma for Beginners</p>
+            <div class="d-flex flex-wrap">
+                <div class="map-block d-flex">
+                    <i class="fa fa-video-camera"></i>
+                    <p>Online Event</p>
+                </div>
+                <ul class="d-flex">
+                    <li>Starts on Jun 01, 2023 5:30 AM</li>
+                    <li>1h</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex contain-checkout-of">
+       <div class="first-contain">
+           <div class="blockImgCheckoutOf">
+               <img src="<?php echo get_stylesheet_directory_uri();?>/img/imgCheckoutOf.png" alt="">
+           </div>
+           <div class="d-flex justify-content-center groupBtn-checkout-of">
+               <button type="button" class="btn btnFavorite">
+                   <i class="fa fa-heart-o"></i>
+                    <!-- <i class="fa fa-heart"></i>-->
+                   Favorite
+               </button>
+               <button class="btn btn-share">
+                   <i class="fa fa-share-alt"></i>
+                   Share
+               </button>
+           </div>
+           <div class="card-info-checkout">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor justo, sodales mattis orci et, mattis faucibus est. Nulla semper consectetur sapien a tempor. Ut vel lacus lorem. Nulla mauris massa, pharetra a mi ut, mattis euismod libero. Ut pretium bibendum urna nec egestas. Etiam tempor vehicula libero. Aenean cursus venenatis orci, ac porttitor leo porta sit amet. Nulla eleifend mollis enim sed rutrum. Nunc cursus ex a ligula consequat aliquet. Donec semper tellus ac ante vestibulum, vitae varius leo mattis. In vestibulum blandit tempus. Etiam elit turpis, volutpat hendrerit varius ut, posuere a sapien. Maecenas molestie bibendum finibus. Nulla euismod neque vel sem hendrerit faucibus. Nam sit amet metus sollicitudin, luctus eros at, consectetur libero.</p>
+               <p>In malesuada luctus libero sed gravida. Suspendisse nunc est, maximus vel viverra nec, suscipit non massa. Maecenas efficitur vestibulum pellentesque. Ut finibus ullamcorper congue. Sed ut libero sit amet lorem venenatis facilisis. </p>
+           </div>
+       </div>
+        <div class="second-contain">
+            <div class="detail-checkout-of">
+                <div class="head-element">
+                    <p>Event Details</p>
+                </div>
+                <div class="content">
+                    <div id="countdown-timer">
+                        <div class="element-contdown">
+                            <strong id="days" class="bold-number">118 </strong> <span class="slim-countdown-text">D<span class="hide-words">ays</span><span class="timer"></span></span>
+                        </div>
+                        <div class="element-contdown">
+                            <strong id="hours" class="bold-number"> 14 </strong> <span class="slim-countdown-text">H<span class="hide-words">ours</span><span class="timer"></span></span>
+                        </div>
+                        <div class="element-contdown">
+                            <strong id="mins" class="bold-number"> 36 </strong> <span class="slim-countdown-text">M<span class="hide-words">inutes</span><span class="timer"></span></span>
+                        </div>
+                        <div class="element-contdown">
+                            <strong id="seconds" class="bold-number"> 24 </strong> <span class="slim-countdown-text">S<span class="hide-words">econds</span></span><span class="timer"></span></span>
+                        </div>
+                    </div>
+                    <div class="block-element-detail">
+                        <div class="sub-detail-circle">
+                            <i class="fas fa-user-circle"></i>
+                        </div>
+                        <div>
+                            <p class="sub-text-1">Organised by :</p>
+                            <p class="sub-text-2">Story Tellers</p>
+                            <a href="" class="sub-text-3">View Profile</a>
+                        </div>
+                    </div>
+                    <div class="block-element-detail">
+                        <div class="sub-detail-circle">
+                            <i class="fas fa-calendar-minus"></i>
+                        </div>
+                        <div>
+                            <p class="sub-text-1">Date and Time</p>
+                            <p class="sub-text-2">Wed, Jun 01, 2022 5:30 AM</p>
+                            <a href="" class="sub-text-3"> <i class="fa fa-calendar"></i> Add to Calendar</a>
+                        </div>
+                    </div>
+                    <div class="block-element-detail">
+                        <div class="sub-detail-circle">
+                            <i class="fas fa-map-marker-alt"></i>
+                        </div>
+                        <div>
+                            <p class="sub-text-1">Location</p>
+                            <p class="sub-text-2">Online</p>
+                        </div>
+                    </div>
+                    <div class="block-element-detail">
+                        <div class="sub-detail-circle">
+                            <i class="fa fa-credit-card"></i>
+                        </div>
+                        <div>
+                            <p class="sub-text-1">AUD</p>
+                            <p class="sub-text-2">$50.00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="detail-checkout-of">
+                <div class="head-element">
+                    <p>Topics</p>
+                </div>
+                <div class="group-topic-course d-flex flex-wrap">
+                    <p>ui design</p>
+                    <p>Tech</p>
+                    <p>Lerning</p>
+                    <p>Figma</p>
+                </div>
+            </div>
+            <div class="detail-checkout-of">
+                <div class="head-element">
+                    <p>Others Course</p>
+                </div>
+                <div class="element-other-course">
+                    <p class="name-other-cours">Artikel N*1</p>
+                    <div class="d-flex flex-wrap">
+                        <p class="tag-category">ux ui design</p>
+                        <p class="tag-category">Figma</p>
+                    </div>
+                </div>
+                <div class="element-other-course">
+                    <p class="name-other-cours">Artikel beyond sky</p>
+                    <div class="d-flex flex-wrap">
+                        <p class="tag-category">ux ui design</p>
+                        <p class="tag-category">Figma</p>
+                    </div>
+                </div>
+                <div class="element-other-course">
+                    <p class="name-other-cours">A beautiful test for if the images are working</p>
+                    <div class="d-flex flex-wrap">
+                        <p class="tag-category">ux ui design</p>
+                        <p class="tag-category">Figma</p>
+                    </div>
+                </div>
+                <a href="" class="btn btn-discover-more">Discover more</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+    const daysEl = document.getElementById('days');
+    const hoursEl = document.getElementById('hours');
+    const minsEL = document.getElementById('mins');
+    const secondsEL = document.getElementById('seconds');
+
+    const newYears = '20 June 2021';
+
+    function countdown() {
+        const newYearsDate = new Date(newYears);
+        const currentDate = new Date();
+
+        const totalSeconds = (newYearsDate - currentDate) /1000;
+        const minutes = Math.floor(totalSeconds/ 60) % 60;
+        const hours = Math.floor(totalSeconds /3600) % 24;
+        const days = Math.floor(totalSeconds /3600/ 24);
+        const seconds = Math.floor(totalSeconds) % 60;
+
+
+        daysEl.innerText = days;
+        hoursEl.innerText = hours;
+        minsEL.innerText = minutes;
+        secondsEL.innerText = seconds;
+
+
+    }
+
+    setInterval(countdown, 1000);
+</script>
+</body>
