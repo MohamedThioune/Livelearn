8d614f06bb3b996ec0efcb333e0571c5fd17b445
pro <pro@mbp-de-pro.home>
2023-04-11 13:07:06 +0000

    user start podcast

--- template-parts/modules/dashboard-user-start-podcast.php
+++ template-parts/modules/dashboard-user-start-podcast.php
@@ -1,11 +1,12 @@
 <html lang="en">
 
-
 <?php wp_head(); ?>
 <?php get_header(); ?>
 
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/template.css" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet"/>
+
+
 <style>
     .theme-side-menu {
         display: none !important;
@@ -13,15 +14,41 @@
     .theme-learning, .theme-form, .theme-content__button-group, .theme-dashboard-blocks {
         padding: 0 !important;
     }
+    .side-bar-course {
+        top: 170px;
+    }
+    .body-content-strat .content-course-strat {
+        margin-top: 180px;
+    }
+    .content-start-course .headBlock a {
+        font-weight: 400;
+        font-size: 14px;
+        color: #000000 !important;
+        margin-bottom: 2px;
+        margin-left: -5px;
+        margin-right: 20px;
+    }
+    .content-list-course ul li i {
+        color: #033356 !important;
+    }
+    @media all and (min-width: 300px) and (max-width: 767px){
+        .theme-content {
+            min-height: 71vh;
+        }
+    }
+
 </style>
 <body>
+
 <div class="content-start-course">
     <div class="headBlock ">
         <div class="d-flex justify-content-between align-items-center">
             <div class="">
-                <a href=""><i class="fa fa-angle-left"></i>Back</a>
-                <p class="title-course">Learn Responsive Web Design Essentials</p>
-                <p class="text-number-element">Video (35)</p>
+                <div class="d-flex align-items-center">
+                    <a href=""><i class="fa fa-angle-left"></i>Back</a>
+                    <p class="title-course">Livelearn Podcast example</p>
+                </div>
+                <p class="text-number-element">List (15)</p>
             </div>
             <p class="percentage-progress-course">2%</p>
         </div>
@@ -46,35 +73,29 @@
                                     <ul class="text-left">
                                         <li>
                                             <a href="">
-                                                <i class="far fa-play-circle" aria-hidden="true"></i>Discover the organization of a network
+                                                <i class="fas fa-microphone-alt" aria-hidden="true"></i><span>01</span> Discover the organization
                                             </a>
                                         </li>
                                         <li>
                                             <a href="">
-                                                <i class="far fa-play-circle" aria-hidden="true"></i>Discover the organization of a network
+                                                <i class="fas fa-microphone-alt" aria-hidden="true"></i><span>02</span> Discover the organization
                                             </a>
                                         </li>
                                         <li>
                                             <a href="">
-                                                <i class="far fa-play-circle" aria-hidden="true"></i>Discover the organization of a network
+                                                <i class="fas fa-microphone-alt" aria-hidden="true"></i><span>03</span> Discover the organization
                                             </a>
                                         </li>
                                         <li>
                                             <a href="">
-                                                <i class="far fa-play-circle" aria-hidden="true"></i>Discover the organization of a network
+                                                <i class="fas fa-microphone-alt" aria-hidden="true"></i><span>04</span> Discover the organization
                                             </a>
                                         </li>
                                         <li>
                                             <a href="">
-                                                <i class="far fa-play-circle" aria-hidden="true"></i>Discover the organization of a network
+                                                <i class="fas fa-microphone-alt" aria-hidden="true"></i><span>05</span> Discover the organization
                                             </a>
                                         </li>
-                                        <li>
-                                            <a href="">
-                                                Conclusion
-                                            </a>
-                                        </li>
-
                                     </ul>
                                 </div>
                             </div>
@@ -146,24 +167,100 @@
             </div>
         </div>
         <div class="content-course-strat position-relative">
-            <video controls>
-                <source src="https://www.youtube.com/embed/dcPp_U-v3bI" title="YouTube video player" allowfullscreen />
-                <source src="https://www.youtube.com/embed/dcPp_U-v3bI" title="livelearn video presentation"  allow="playsinline;" type="video/ogg" /><!-- Firefox / Opera / Chrome10 -->
-            </video>
-            <div class="d-flex justify-content-between prev-next-btn">
-                <!--  <a href="" class="btn btn-prev">
-                       <i class="fa fa-angle-left"></i>
-                       text ici
-                </a>-->
+
+          <div>
+              <div class="imgPadcastCourse">
+                  <img src="<?php echo get_stylesheet_directory_uri();?>/img/assessment-2.png" alt="">
+              </div>
+              <div class="sound-wave">
+                  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" preserveAspectRatio="none" viewBox="0 0 1440 560">
+                      <g mask='url("#SvgjsMask1099")' fill="none">
+                          <rect fill="#0e2a47"></rect>
+                          <g transform="translate(0, 0)" stroke-linecap="round" stroke="url(#SvgjsLinearGradient1100)">
+                              <path d="M375 202.15 L375 357.85" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M398 155.33 L398 404.67" stroke-width="17.25" class="bar-scale3"></path>
+                              <path d="M421 196.44 L421 363.56" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M444 259.91 L444 300.09" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M467 208.25 L467 351.75" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M490 184.8 L490 375.2" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M513 249.28 L513 310.72" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M536 220.75 L536 339.25" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M559 254.8 L559 305.2" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M582 186.77 L582 373.23" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M605 210.13 L605 349.87" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M628 234.45 L628 325.55" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M651 241.1 L651 318.89" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M674 202.95 L674 357.05" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M697 165.81 L697 394.19" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M720 224.51 L720 335.49" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M743 157.59 L743 402.4" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M766 164.98 L766 395.02" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M789 158.93 L789 401.07" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M812 224.24 L812 335.76" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M835 171.73 L835 388.27" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M858 264.89 L858 295.11" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M881 175.14 L881 384.86" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M904 248.17 L904 311.83" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M927 185.4 L927 374.6" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M950 234.82 L950 325.18" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M973 229.9 L973 330.1" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M996 194.25 L996 365.75" stroke-width="17.25" class="bar-scale2 "></path>
+                              <path d="M1019 162.47 L1019 397.53" stroke-width="17.25" class="bar-scale1 "></path>
+                              <path d="M1042 205.06 L1042 354.94" stroke-width="17.25" class="bar-scale3 "></path>
+                              <path d="M1065 240.52 L1065 319.48" stroke-width="17.25" class="bar-scale1 "></path>
+                          </g>
+                      </g>
+                      <defs>
+                          <mask id="SvgjsMask1099">
+                              <rect width="1440" height="560" fill="#ffffff"></rect>
+                          </mask>
+                          <linearGradient x1="360" y1="280" x2="1080" y2="280" gradientUnits="userSpaceOnUse" id="SvgjsLinearGradient1100">
+                              <stop stop-color="#3a7cc3" offset="0"></stop>
+                              <stop stop-color="#dd1133" offset="1"></stop>
+                          </linearGradient>
+                      </defs>
+                  </svg>
+              </div>
+          </div>
+
+           <!-- <div class="d-flex justify-content-between prev-next-btn">
                 <a href="" class="btn btn-next ml-auto">
                     I've finished this video I'll continue
                     <i class="fa fa-angle-right"></i>
                 </a>
+            </div>-->
+        </div>
+    </div>
+    <div class="audioBar">
+        <div class="container-fluid">
+            <div class="d-flex align-items-center text-center">
+                <button class="codeless-player-toggle">
+                    <img id="down-chevron" src="<?php echo get_stylesheet_directory_uri();?>/img/down-chevron.svg" alt="">
+                    <img id="up-chevron" src="<?php echo get_stylesheet_directory_uri();?>/img/up-chevron.png" alt="">
+                </button>
+                <div class="codeless-player-info codeless-element">
+                    <div class="codeless-player-nav">
+                        <button class="previous round livecast-play">&#8249;</button>
+                        <button class="next round livecast-play">&#8250;</button>
+                    </div>
+                    <div class="codeless-player-content">
+                        <h5>Discover the organization</h5>
+                    </div>
+                </div>
+               <div class="codeless-player-audio  codeless-element">
+                    <audio controls id="myAudioID">
+                        <source src="<?php echo get_stylesheet_directory_uri();?>/sounds/firstSounds.mp3" type="audio/ogg">
+                        <source src="<?php echo get_stylesheet_directory_uri();?>/sounds/firstSounds.mp3" type="audio/mpeg">
+                        <source src="<?php echo get_stylesheet_directory_uri();?>/sounds/firstSounds.mp3" type="audio/aac">
+                        <source src="<?php echo get_stylesheet_directory_uri();?>/sounds/firstSounds.mp3" type="audio/wav">
+                        <source src="<?php echo get_stylesheet_directory_uri();?>/sounds/firstSounds.mp3" type="audio/aiff">
+                        Your browser does not support the audio element.
+                    </audio>
+                </div>
             </div>
         </div>
     </div>
-</div>`
-
+</div>
 
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -188,13 +285,21 @@
 </script>
 <script>
     $(document).ready(function() {
-        $(".btn-show-list-course").click(function() {
-            $(".side-bar-course").show();
+        var audio = document.querySelector('audio');
+        $(".sound-wave").hide();
+        audio.onplay = function() {
+            audio.play(0);
+            $(".imgPadcastCourse").addClass("hide");
+            $(".sound-wave").show();
+        };
+
+        audio.addEventListener('pause', (event) => {
+            $(".imgPadcastCourse").removeClass("hide");
+            $(".sound-wave").hide();
         });
-        $(".btn-hide-bar").click(function() {
-            $(".side-bar-course").hide();
-        });
-    });
+
+
+    } )
 </script>
 
 </body>
