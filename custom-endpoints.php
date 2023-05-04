d610db818d8c60275d102b8598c23b692bae8c76
fadel221 <gueyefadel22@gmail.com>
2023-05-04 10:55:47 +0000

    add users images on communities endpoints

--- custom-endpoints.php
+++ custom-endpoints.php
@@ -1219,7 +1219,7 @@ function getCommunityById($data)
   $community = get_post($id_community) ?? null;
 
   if ($community == null)
-  return ["error" => "This community does not exist !"];
+    return ["error" => "This community does not exist !"];
   
     $community-> author_company = get_field('company_author',$community->ID) ? get_field('company_author',$community->ID) : null;
     $community->image_community = get_field('image_community',$community->ID) ? get_field('image_community',$community->ID) : null;
@@ -1234,8 +1234,8 @@ function getCommunityById($data)
         if ($follower -> data -> ID == $user_id)
           $community->is_connected_user_member = true;
           $follower -> data ->profile_image =  get_field('profile_img','user_'.(int)$follower -> data ->ID) != false ? get_field('profile_img','user_'.(int)$follower -> data ->ID) : get_stylesheet_directory_uri() . '/img/placeholder_user.png';
-        $follower -> data ->role = get_field('role', 'user_' . (int)$follower -> data ->ID) ? get_field('role', 'user_' . (int)$follower -> data ->ID) : '';
-        array_push($community->followers, $follower -> data);
+          $follower -> data ->role = get_field('role', 'user_' . (int)$follower -> data ->ID) ? get_field('role', 'user_' . (int)$follower -> data ->ID) : '';
+          array_push($community->followers, $follower -> data);
       }
 
     $community -> questions = get_field('question_community',$community->ID) ? get_field('question_community',$community->ID) : [];
@@ -1365,6 +1365,7 @@ function askQuestion(WP_REST_Request $request)
     //New question
     $question_community = get_field('question_community', $community_id) ? get_field('question_community', $community_id) : [] ;
     $question['user_question'] = $user;
+    $question['user_question']->data->profile_image = get_field('profile_img','user_'.(int)$question['user_question']->data->ID) != false ? get_field('profile_img','user_'.(int)$question['user_question']->data->ID) : get_stylesheet_directory_uri() . '/img/placeholder_user.png';
     $question['text_question'] = $text_question;
     array_push($question_community, $question);
 
@@ -1409,8 +1410,8 @@ function replyQuestion(WP_REST_Request $request)
             $reply = array();
             $user_reply = $user;
             $reply['user_reply'] = $user_reply;
+            $reply['user_reply']->data->profile_image = get_field('profile_img','user_'.(int)$reply['user_reply']->data->ID) != false ? get_field('profile_img','user_'.(int)$reply['user_reply']->data->ID) : get_stylesheet_directory_uri() . '/img/placeholder_user.png';
             $reply['text_reply'] = $text_reply;
-
             if(empty($question_community[$index_question]['reply_question']))
                 $question_community[$index_question]['reply_question'] = array();
 
@@ -1453,144 +1454,6 @@ function replyQuestion(WP_REST_Request $request)
       }
   }
   /** Views Endpoints */
-  function visibility($data, $course, $visibility_company)
-  {
-    $bool = true;
-    $invisibility = get_field('visibility', $course->ID);
-
-    $company = get_field('company',  'user_' . $course->post_author);
-    if(!empty($company))
-        $company_title = $company[0]->post_title;
-
-    if($invisibility && $visibility_company != $company_title )
-        $bool = false;
-
-    return $bool;
-}
-
-function view($course, $user_visibility)
-{
-    $user_id = (isset($user_visibility->ID)) ? $user_visibility->ID : 0;
-    if(!$user_id)
-        return false;
-    $args = array(
-        'post_type' => 'view', 
-        'post_status' => 'publish',
-        'author' => $user_id,
-    );
-
-    $views_stat_user = get_posts($args);
-
-    if(!empty($views_stat_user))
-        $stat_id = $views_stat_user[0]->ID;
-    else{
-        $data = array(
-            'post_type' => 'view',
-            'post_author' => $user_id,
-            'post_status' => 'publish',
-            'post_title' => $user_visibility->display_name . ' - View',
-            );
-        
-        $stat_id = wp_insert_post($data);
-    }
-
-    $view = get_field('views', $stat_id);
-    
-    $one_view = array();
-    $one_view['course'] = $course;
-    $one_view['date'] = date('d/m/Y H:i:s');
-
-    if(!empty($view))
-        array_push($view, $one_view);
-    else 
-        $view = array($one_view); 
-    
-    update_field('views', $view, $stat_id);
-
-}
-
-function view_topic($topic_id, $user_visibility){
-    $user_id = (isset($user_visibility->ID)) ? $user_visibility->ID : 0;         
-    if(!$user_id)
-        return false;
-
-    $args = array(
-        'post_type' => 'view', 
-        'post_status' => 'publish',
-        'author' => $user_id,
-    );
-
-    $views_stat_user = get_posts($args);
-
-    if(!empty($views_stat_user))
-        $stat_id = $views_stat_user[0]->ID;
-    else{
-        $data = array(
-            'post_type' => 'view',
-            'post_author' => $user_id,
-            'post_status' => 'publish',
-            'post_title' => $user_visibility->display_name . ' - View',
-            );
-        
-        $stat_id = wp_insert_post($data);
-    }
-
-    $view = get_field('views_topic', $stat_id);
-    
-    $one_view = array();
-    $one_view['view_id'] = $topic_id;
-    $one_view['view_name'] = (String)get_the_category_by_ID($topic_id);
-    $one_view['view_date'] = date('d/m/Y H:i:s');
-
-    if(!empty($view))
-        array_push($view, $one_view);
-    else 
-        $view = array($one_view); 
-    
-    update_field('views_topic', $view, $stat_id);
-
-}
-
-function view_user($expert_id, $user_visibility){
-    $user_id = (isset($user_visibility->ID)) ? $user_visibility->ID : 0;
-    if(!$user_id)
-        return false;
-
-    $args = array(
-        'post_type' => 'view', 
-        'post_status' => 'publish',
-        'author' => $user_id,
-    );
-
-    $views_stat_user = get_posts($args);
-
-    if(!empty($views_stat_user))
-        $stat_id = $views_stat_user[0]->ID;
-    else{
-        $data = array(
-            'post_type' => 'view',
-            'post_author' => $user_id,
-            'post_status' => 'publish',
-            'post_title' => $user_visibility->display_name . ' - View',
-            );
-        
-        $stat_id = wp_insert_post($data);
-    }
-
-    $view = get_field('views_user', $stat_id);
-    
-    $one_view = array();
-    $one_view['view_id'] = $expert_id;
-    $one_view['view_name'] = get_userdata($expert_id)->display_name;
-    $one_view['view_date'] = date('d/m/Y H:i:s');
-
-    if(!empty($view))
-        array_push($view, $one_view);
-    else 
-        $view = array($one_view); 
-    
-    update_field('views_user', $view, $stat_id);
-
-}
+  
 
 /** Views Endpoints */
