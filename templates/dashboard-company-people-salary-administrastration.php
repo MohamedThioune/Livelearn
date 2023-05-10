4724c54975038998789a46233136ce691aa634e3
Khadim NIASS <niassssn@gmail.com>
2023-05-10 14:38:30 +0000

    make algorithme of mersen

--- templates/dashboard-company-people-salary-administrastration.php
+++ templates/dashboard-company-people-salary-administrastration.php
@@ -2,6 +2,22 @@
 <?php // /livelearn/salaryadmin ?>
 
 <?php
+$users = get_users();
+extract($_POST);
+    if (isset($managers)){
+        // var_dump($managers); 
+        foreach($managers as $man):
+            $link = "/dashboard/company/profile/?id=" . $man->ID . '&manager='. $man->ID;
+            $img_manager = get_field('profile_img',  'user_' . $man->ID) ? get_field('profile_img',  'user_' . $man->ID) : get_stylesheet_directory_uri() . '/img/placeholder_user.png';
+        ?>
+        <tr>
+            <td> <?php echo $man->first_name!='' ? $man->first_name : $man->display_name ?> </td>
+            <td>
+                <img class="" src="<?= $img_manager ?>" alt="">
+            </td>
+            <td><a href="<?= $link ?>">See</a></td>
+        </tr>
+        <?php endforeach ?>
 
-?>
-<h1>Hello template salary admin</h1>
+        <?php } ?>
+   
