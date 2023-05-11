b27950d66633e0a578a52b9c63ca686ef037c077
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-05-11 13:27:53 +0000

    cookie modal home

--- templates/apply-cookie.php
+++ templates/apply-cookie.php
@@ -6,11 +6,11 @@
     extract($_POST);
     //Instructions should be there ...
     if($set_cookie == "mobile_download") 
-        setcookie('mobile_download', '1', time()+3600 * 24 * 7 , '/');
+    setcookie('mycookie','mydata',time() + 2*7*24*60*60,'/','.livelearn.nl', false);
+        setcookie('mobile_download', '1', time()+3600 * 24 * 7, '/','.livelearn.nl', false);
     else if($set_cookie == "general") 
-        setcookie('general', $cookie_value, time()+3600 * 24 * 360 , '/');
+        setcookie('general', $cookie_value, time()+3600 * 24 * 360 , '/','.livelearn.nl', false);
     if(isset($cookie_value))
         if($cookie_value) 
             echo $cookie_value;
-        // echo '<h1 class="wordDeBestText2">Preferences set correctly</h1>';
 ?>
