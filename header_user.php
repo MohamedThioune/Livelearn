c6bdf95dea756f49fbbb6aa9b28ac37ffef7672e
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-06-08 11:24:06 +0000

    Google tag manager --instrcutions

--- header_user.php
+++ header_user.php
@@ -46,6 +46,15 @@ $see_experts = get_users(
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5NTJ5Z4');
+        </script>
+        <!-- End Google Tag Manager -->
         <meta name="description" content="Fluidify">
         <meta name='keywords' content="fluidify">
         <meta charset="utf-8">
@@ -99,8 +108,13 @@ $see_experts = get_users(
         <?php wp_head(); ?>
     </head>
     <body class="header-user canhas">
-
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NTJ5Z4"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     <!-- Modal -->
     <div class="modal fade  modal-dialog-scrollable" id="bedrijfsprofiel_modal" tabindex="-1" role="dialog" aria-labelledby="bedrijfsprofiel_modalModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
