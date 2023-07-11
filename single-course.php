13db59e7464496b1539cb5185b8a8ab06fea88e5
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-07-11 15:45:48 +0000

    Podcast

--- single-course.php
+++ single-course.php
@@ -248,7 +248,7 @@ if(in_array($course_type, $offline))
 else if(in_array($course_type, $online))
     include_once('template-parts/modules/single-new-course-video.php');
 else if($course_type == 'Podcast')
-    include_once('template-parts/modules/single-new-course-podcast.php');
+    include_once('template-parts/modules/single-course-podcast.php');
 
 ?>  
  
