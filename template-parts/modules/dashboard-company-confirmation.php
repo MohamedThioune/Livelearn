ed59e079c1f91dc13b0356847d679ac51125c1db
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-08-29 16:17:32 +0000

    Dashboard --subscription

--- template-parts/modules/dashboard-company-confirmation.php
+++ template-parts/modules/dashboard-company-confirmation.php
@@ -1,4 +1,5 @@
 <?php
+    var_dump($abonnement->billing);
     if(isset($abonnement->invoices))
         if($abonnement->invoices):
             $last_order = array_reverse($abonnement->invoices)[0];
