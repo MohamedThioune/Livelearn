433085c2d3c42e2fa386aefe2540a2e88dd7a525
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-08-31 22:08:44 +0000

    Badges --draft

--- custom-endpoints.php
+++ custom-endpoints.php
@@ -2008,6 +2008,7 @@ function save_user_views(WP_REST_Request $request)
     global $wpdb;
     $table_tracker_views = $wpdb->prefix . 'tracker_views';
     $occurence = 1;
+
     //Add by MaxBird - get name entity
     if($data_type == 'course')
     {
@@ -2021,6 +2022,110 @@ function save_user_views(WP_REST_Request $request)
     }
     else if($data_type == 'topic')
         $data_name = (String)get_the_category_by_ID($data_id);
+
+    /** Badges **/
+    $sql = $wpdb->prepare( "SELECT data_id FROM $table_tracker_views WHERE user_id = $user_id");
+    $occurences = $wpdb->get_results( $sql );
+    $count = array('Opleidingen' => 0, 'Workshop' => 0, 'E-learning' => 0, 'Event' => 0, 'E_learning' => 0, 'Training' => 0, 'Video' => 0, 'Artikel' => 0, 'Podcast' => 0);
+    $libelle_badges = [
+        'Congratulations ' . $user_visibility->display_name . ', you\'ve just read your first article !',
+        'Well done ' . $user_visibility->display_name . ' you become expert in article',
+        'Good job ' . $user_visibility->display_name . ', video expert apprentice !',
+        'Well done ' . $user_visibility->display_name . ' you become expert in video',
+        $user_visibility->display_name . ', you\'re really a podcast enthusiast !' 
+    ];
+    $trigger_badges = [
+        'Read my first article !',
+        'Read 50 articles !',
+        'Read 10 videos !',
+        'Read 50 videos !' ,
+        'Read 30 podcasts !' 
+    ];
+    $array_badges = array();
+
+    foreach ($occurences as $occurence) {
+        $course_type = get_field('course_type', $occurence->data_id);
+        $count[$course_type]++;
+    }
+
+    $image_badge = get_stylesheet_directory_uri() . '/img/badge-assessment-sucess.png';
+    $trigger_badge = null;
+    if($count['Artikel'] >= 1 && $count['Artikel'] < 50){
+        $array_badge = array();
+        $array_badge['libelle'] = $libelle_badges[0];
+        $array_badge['image'] = $image_badge;
+        $array_badge['trigger'] = $trigger_badges[0];
+        $object_badge = (Object)$array_badge;
+        array_push($array_badges, $object_badge);
+    }
+    if($count['Artikel'] >= 50){
+        $array_badge = array();
+        $array_badge['libelle'] = $libelle_badges[1];
+        $array_badge['image'] = $image_badge;
+        $array_badge['trigger'] = $trigger_badges[1];
+        $object_badge = (Object)$array_badge;
+        array_push($array_badges, $object_badge);
+    }
+    if($count['Video'] >= 10 && $count['Video'] < 50){
+        $array_badge = array();
+        $array_badge['libelle'] = $libelle_badges[2];
+        $array_badge['image'] = $image_badge;
+        $array_badge['trigger'] = $trigger_badges[2];
+        $object_badge = (Object)$array_badge;
+        array_push($array_badges, $object_badge);
+    }
+    if($count['Video'] >= 50){
+        $array_badge = array();
+        $array_badge['libelle'] = $libelle_badges[3];
+        $array_badge['image'] = $image_badge;
+        $array_badge['trigger'] = $trigger_badges[3];
+        $object_badge = (Object)$array_badge;
+        array_push($array_badges, $object_badge);
+    }
+    if($count['Podcast'] >= 7){
+        $array_badge = array();
+        $array_badge['libelle'] = $libelle_badges[4];
+        $array_badge['image'] = $image_badge;
+        $array_badge['trigger'] = $trigger_badges[4];
+        $object_badge = (Object)$array_badge;
+        array_push($array_badges, $object_badge);
+    }
+
+    foreach($array_badges as $badge)
+      if($badge){
+        //Occurrence check
+        $args = array(
+            'post_type' => 'badge', 
+            'title' => $badge->libelle,
+            'post_status' => 'publish',
+            'author' => $user_id,
+            'posts_per_page'         => 1,
+            'no_found_rows'          => true,
+            'ignore_sticky_posts'    => true,
+            'update_post_term_cache' => false,
+            'update_post_meta_cache' => false
+        );
+        $badges = get_posts($args);
+
+        if(empty($badges)){
+            $post_data = array(
+                'post_title' => $badge->libelle,
+                'post_author' => $user_id,
+                'post_type' => 'badge',
+                'post_status' => 'publish'
+            );
+            $badge_id = wp_insert_post($post_data);
+        }
+
+        if(isset($badge_id))
+            if($badge_id){
+                update_field('image_badge', $badge->image, $badge_id);
+                update_field('trigger_badge', $badge->trigger, $badge_id);
+            }
+      } 
+
+    /** end Badges */
+
     //testing wheither data_id exist ?
     $sql = $wpdb->prepare( "SELECT occurence FROM $table_tracker_views  WHERE data_id = $data_id");
     $occurence_id = $wpdb->get_results( $sql)[0]->occurence;
