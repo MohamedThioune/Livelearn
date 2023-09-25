f158548b63505e71df9b2f8d82b8f51c386301b2
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-09-25 09:51:27 +0000

    latest setup for youtube and XML

--- functions.php
+++ functions.php
@@ -1827,7 +1827,7 @@ add_action( 'rest_api_init', function () {
      'callback' => 'Artikel_From_Company'
   ));
 
-  register_rest_route ('custom/v1', '/xml', array(
+  register_rest_route ('custom/v1', '/xml/(?P<id>\d+)', array(
     'methods' => 'GET',
     'callback' => 'xmlParse'
  ));
