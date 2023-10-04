141e995f8c1ed9e76b7d752e1cb290bf0ecab6e9
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-10-04 03:11:08 +0000

    Dashboard profile + worflow notifications

--- functions.php
+++ functions.php
@@ -563,6 +563,50 @@ function custom_post_type() {
 
     register_post_type( 'badge', $badge_args );
 
+    //To Do's with
+    $todo = array(
+        'name'                => _x( 'Todos', 'Todos', 'todo' ),
+        'singular_name'       => _x( 'Todos', 'Todo', 'todo' ),
+        'menu_name'           => __( 'Todos', 'todo' ),
+        //'parent_item_colon'   => __( 'Parent Item:', 'fdfd_issue' ),
+        'all_items'           => __( 'All todos', 'todo' ),
+        'view_item'           => __( 'View todo', 'view_todo' ),
+        'add_new_item'        => __( 'New todo', 'add_new_todo' ),
+        'add_new'             => __( 'New todo', 'text_domain' ),
+        'edit_item'           => __( 'Edit Item', 'text_domain' ),
+        'update_item'         => __( 'Update Item', 'text_domain' ),
+        'search_items'        => __( 'Search Item', 'text_domain' ),
+        'not_found'           => __( 'Not found', 'text_domain' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+    );
+
+    $todo_args = array(
+        'label'               => __( 'todo', 'text_domain' ),
+        'description'         => __( 'Post type for fdfd issue', 'text_domain' ),
+        'labels'              => $todo,
+        'supports'            => array('title', 'editor', 'author', 'custom-fields', 'excerpt'),
+        //'taxonomies'          => array('sales-person', 'sales-margin', 'location' ),
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_rest'        => false,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'menu_icon'           => '',
+        'can_export'          => true,
+        'rewrite'             => array('slug' => 'todo'),
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'page',
+
+    );
+
+    register_post_type( 'todo', $todo_args );
+
+
 }
 add_action( 'init', 'custom_post_type', 0 );
 
