f89ff33b8107c3854448c0234ad5243f493fe3ae
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-10-10 04:33:23 +0000

    agile scrum test

--- article-endpoints.php
+++ article-endpoints.php
@@ -346,11 +346,11 @@ function xmlParse()
     //Get all users
     $users = get_users();
 
-    $website_urls = ['2xplain-b.v-20230925.0140.xml',
-      'agile-scrum-group-20230922.1323.xml',
-      'anker-kompas-20230922.1323.xml', 
-      'aeres-tech-20230925.0141.xml', 
-      'academie-voor-arbeidsmarktcommunicatie-b.v-20230925.0141.xml'];
+    $website_urls = [/*'2xplain-b.v-20230925.0140.xml',*/
+        'agile-scrum-group-20230922.1323.xml'/*,
+        'anker-kompas-20230922.1323.xml', 
+        'aeres-tech-20230925.0141.xml', 
+    'academie-voor-arbeidsmarktcommunicatie-b.v-20230925.0141.xml'*/];
 
     //Start inserting course
     echo "<h1 class='titleGroupText' style='font-weight:bold'>SCRIPT XML PARSING</h1>";
