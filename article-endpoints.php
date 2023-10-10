fdfb68d688d137b0f30086c57061e37caeafa111
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-10-10 03:30:21 +0000

    update company issue

--- article-endpoints.php
+++ article-endpoints.php
@@ -425,7 +425,7 @@ function xmlParse()
             foreach ($users as $user) {
                 $company_user = get_field('company', 'user_' . $user->ID);
 
-                if (strtolower($company_user[0]->post_title) == strtolower(strval($post['org']))) {
+                if (trim(strtolower($company_user[0]->post_title)) == trim(strtolower(strval($post['org'])))) {
                     $author_id = $user->ID;
                     $company = $company_user[0];
                     $company_id = $company_user[0]->ID;
@@ -440,7 +440,7 @@ function xmlParse()
 
                 $companies = get_posts($args);
                 foreach ($companies as $value) {
-                    if (strtolower($value->post_title) == strval($post['org'])) {
+                    if (trim(strtolower($value->post_title)) == trim(strval($post['org'])) ) {
                         $company = $value;
                         $company_id = $value->ID;
                         break;
