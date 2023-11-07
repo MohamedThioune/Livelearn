0eb89340be744111c5aa440726f2a4e83b320161
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-11-07 03:20:26 +0000

    Search natural + merge Megamind Xadim

--- templates/template-product-search.php
+++ templates/template-product-search.php
@@ -1,3 +1,112 @@
+<?php /** Template Name: Product Search */ ?>
+
+<body>
+<?php wp_head(); ?>
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/template.css" />
+<?php
+$page = 'check_visibility.php';
+require($page);
+
+//Modules 
+require_once('postal.php'); 
+require_once('search-module.php'); 
+
+//Let's kick it off
+extract($_GET);
+
+// extract($_POST);   
+
+$courses = array();
+$order_type = array();
+
+// $teachers = array();
+// $categories = array();
+// $profes = array();
+// $topic = "";
+
+if (!isset($leervom))
+    $leervom = array();
+if (!isset($experties))
+    $experties = array();
+
+$args = array(
+    'post_type' => array('post','course'),
+    'post_status' => 'publish',
+    'orderby' => 'date',
+    'order'   => 'DESC',
+    'posts_per_page' => -1,
+);
+$global_posts = get_posts($args);
+
+$no_filter = true;
+
+//Natural Search 
+if(isset($search) && isset($search_type)):
+    $courses = searching_course($search, $search_type, $global_posts)['courses'];
+    $order_type = searching_course($search, $search_type, $global_posts)['order_type'];
+    $expertise = searching_course($search, $search_type, $global_posts)['experts'];
+    $no_filter = false;
+//Head Search
+elseif($filter):
+    $courses = searching_course_by_type($global_posts, $filter)['courses'];
+    $order_type = searching_course_by_type($global_posts, $filter)['order_type'];
+    $expertise = searching_course_by_type($global_posts, $filter)['experts'];    
+    $no_filter = false;
+endif;
+
+/* * Group by type * */
+$main_courses = array();
+$start_courses = (!empty($courses)) ? $courses : $global_posts;
+// $category_input = 216;
+// $user_input = 3;
+// $companie_input = 1435;
+
+if(isset($category_input) || isset($user_input) || isset($companie_input))
+    $no_filter = false;
+
+// Category post 
+if(isset($category_input)):
+    $courses = searching_course_by_group($start_courses, 'category', $category_input)['courses'];
+    $order_type = searching_course_by_group($start_courses, 'category', $category_input)['order_type'];
+    $expertise = searching_course_by_group($start_courses, 'category', $category_input)['experts'];
+// Expert post 
+elseif(isset($user_input)):
+    $courses = searching_course_by_group($start_courses, 'expert', $user_input)['courses'];
+    $order_type = searching_course_by_group($start_courses, 'expert', $user_input)['order_type'];
+    $expertise = searching_course_by_group($start_courses, 'category', $category_input)['experts'];
+// Company post 
+elseif(isset($companie_input)):
+    $courses = searching_course_by_group($start_courses, 'company', $companie_input)['courses'];
+    $order_type = searching_course_by_group($start_courses, 'company', $companie_input)['order_type'];
+    $expertise = searching_course_by_group($start_courses, 'category', $category_input)['experts'];
+endif;
+/* * End group by * */
+
+/* * Search by filter * */
+if(isset($filter_args)):
+    $no_filter = false;
+
+    // Define args for search
+    $args = array();
+    $args['leervom'] = (!empty($leervom)) ? $leervom : null;
+    $args['min'] = ($min) ? $min : null;
+    $args['max'] = ($max) ? $max : null;
+    $args['gratis'] = (isset($gratis)) ? 1 : null;
+    $args['locate'] = ($locate) ? $locate : null;
+    $args['online'] = (isset($online)) ? 1 : null;
+    $args['experties'] = (!empty($experties)) ? $experties : null;
+  
+    //Apply filter 
+    $courses = searching_course_with_filter($courses, $args)['courses']; 
+    $order_type = searching_course_with_filter($courses, $args)['order_type'];
+    $expertise = searching_course_with_filter($courses, $args)['experts'];
+    /* * End search by * */
+endif;
+
+//Check empty 
+$courses = ($no_filter) ? $global_posts : $courses;
+?>
 
 <div class="content-community-overview bg-gray">
     <section class="boxOne3-1">
@@ -16,7 +125,206 @@
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Close.png" class="img-head-about" alt="">
                     </button>
                     <p class="text-filter-course">Filter | Courses </p>
-                    
+                    <form action="/product-search" method="GET">
+                        <?php
+                        echo "<input type='hidden' name='filter_args' value='1'>";
+
+                        if(isset($_GET['category_input']))
+                            echo "<input type='hidden' name='category_input' value='". $category_input ."'>";
+                        elseif(isset($_GET['user_input']))
+                            echo "<input type='hidden' name='user_input' value='". $user_input ."'>";
+                        elseif(isset($_GET['companie_input']))
+                            echo "<input type='hidden' name='companie_input' value='". $companie_input ."'>";
+                        ?>
+                        <div class="sub-section">
+                            <p class="description-sub-section">LEERVORM</p>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="opleiding" name="leervom[]" value="Opleidingen" <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Opleidingen', $leervom) || $search_type == "Opleidingen" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Opleidingen
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Opleidingen']) ? $order_type['Opleidingen'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Artikel"  <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Artikel', $leervom) || $search_type == "Artikel" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Artikel
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Artikel']) ? $order_type['Artikel'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Masterclass" <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Masterclass', $leervom) || $search_type == "Masterclass" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Masterclass
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Masterclass']) ? $order_type['Masterclass'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Podcast" <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Podcast', $leervom) || $search_type == "Podcast" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Podcast
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Podcast']) ? $order_type['Podcast'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Video" <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Video', $leervom) || $search_type == "Video" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Video
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Video']) ? $order_type['Video'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Workshop" <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Workshop', $leervom) || $search_type == "Workshop" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Workshop
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Workshop']) ? $order_type['Workshop'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="E-Learning" <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('E-Learning', $leervom) || $search_type == "E-Learning" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        E-Learning
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['E-Learning']) ? $order_type['E-Learning'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Event"  <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Event', $leervom) || $search_type == "Event" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Event
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Event']) ? $order_type['Event'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Training"  <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Training', $leervom) || $search_type == "Training" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Training
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Training']) ? $order_type['Training'] : 0 ?></p>
+                            </div>
+
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="" name="leervom[]" value="Lezing" <?php if( !empty($leervom) ||  isset($search_type) ) if(in_array('Lezing', $leervom) || $search_type == "Lezing" ) echo "checked" ; else echo ""  ?>>
+                                    <label class="form-check-label" for="Backend">
+                                        Lezing
+                                    </label>
+                                </div>
+                                <p class="number-course"><?= isset($order_type['Lezing']) ? $order_type['Lezing'] : 0 ?></p>
+                            </div>
+                        </div>
+
+                        <div class="sub-section">
+                            <p class="description-sub-section">PRIJS</p>
+                            <div class="form-group">
+                                <label for="Vanaf" class="form-label">Vanaf</label>
+                                <input type="number" class="form-control" id="Vanaf" name="min" value="<?php if(isset($min)) echo $min ?>"  placeholder="€min">
+                            </div>
+                            <div class="form-group">
+                                <label for="Tot" class="form-label">Tot</label>
+                                <input type="number" class="form-control" id="Tot" name="max" value="<?php if(isset($max)) echo $max ?>"  placeholder="€max">
+                            </div>
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" id="Allen" name="gratis" <?php if(isset($gratis)) echo 'checked'; else  echo  '' ?>>
+                                    <label class="form-check-label" for="Allen">
+                                        Alleen gratis
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <div class="sub-section">
+                            <p class="description-sub-section">LOCATIE</p>
+                            <div class="form-group">
+                                <label for="PostCode" class="form-label">PostCode</label>
+                                <input type="number" class="form-control" id="PostCode" name="locate" placeholder="&nbsp;Postcode">
+                            </div>
+                            <div class="form-group">
+                                <label for="Afstand" class="form-label">Afstand(m)</label>
+                                <input type="number" class="form-control" id="Afstand" name="range" placeholder="&nbsp;Afstand(m)">
+                            </div>
+                            <div class="form-check d-flex justify-content-between">
+                                <div class="group-input-check">
+                                    <input class="form-check-input" type="checkbox" name="online" id="Alleen-online"  name="gratis" <?php if(isset($online)) echo 'checked'; else  echo  '' ?>>
+                                    <label class="form-check-label" for="Alleen-online">
+                                        Alleen online
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="sub-section">
+                            <p class="description-sub-section">EXPERT</p>
+                            <!-- <div class="position-relative">
+                                <i class="fa fa-search"></i>
+                                <input type="search" placeholder="Search Expert">
+                            </div> -->
+                            <br>
+                            <div class="form-check">
+                            <?php
+                            $i = 0;
+                            foreach($expertise as $expert):
+                                if(!$expert)
+                                    continue;
+
+                                $teacher_data = get_user_by('id', $expert);
+
+                                $user_id = get_current_user_id();
+                                if($teacher_data->ID != $user_id)
+                                    $name = ($teacher_data->last_name) ? $teacher_data->first_name : $teacher_data->display_name;
+                                else
+                                    $name = "Ikzelf";
+    
+                                if($teacher_data->first_name == "")
+                                    continue;
+                                ?>
+                                <div class="group-input-check">
+                                    <input class="form-check-input" name="experties[]" type="checkbox" value="<?= $expert ?>" id="">
+                                    <label class="form-check-label" for="">
+                                        <?= $name ?>
+                                    </label>
+                                </div>
+                            <?php
+                                $i+=1;
+                                if($i == 10)
+                                    break;
+                            endforeach;  
+
+                            if(!empty($expertise))
+                                if(count($expertise) > 10)
+                                    echo '<button class="btn btn-more-expert">+ More expert</button>';
+                            ?>
+                            </div>
+                        </div>
+                        <button class="btn btn-Apply">Apply</button>
+                    </form>
 
                 </div>
             </div>
