cd7871eb600ef1c35cb5c38f3a0dd22e0e5f5760
Ibrahima Faye <ibouddha1.if@gmail.com>
2023-12-07 06:35:19 +0000

    debug 2

--- templates/template-xml-parse.php
+++ templates/template-xml-parse.php
@@ -51,7 +51,7 @@
     $file = get_stylesheet_directory_uri() . "/" . $website ;
     $xml = simplexml_load_file($file);
     $data_xml = $xml->program;
-    var_dump($data_xml);
+    var_dump($file);
 
 
     echo "<h3>".$data_xml[0]->programClassification->orgUnitId." running <i class='fas fa-spinner fa-pulse'></i></h3><br><br>";
