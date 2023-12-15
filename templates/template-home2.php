361600168c112d0a193582bbe6ba826d87c74028
Mohamed Thioune <mokhouthioune96@gmail.com>
2023-12-15 19:38:47 +0000

    Home2 --push search

--- templates/template-home2.php
+++ templates/template-home2.php
@@ -1010,7 +1010,7 @@ $saved = get_user_meta($user_id, 'course');
 
                 <h1 class="wordDeBestText2">Hét leer- en upskilling platform van- én voor de toekomst</h1>
                 <p class="altijdText2">Onhandig als medewerkers niet optimaal functioneren. LiveLearn zorgt dat jouw workforce altijd op de hoogte is van de laatste kennis en vaardigheden.</p>
-                <form action="/product-search" class="position-relative newFormSarchBar" method="POST">
+                <form action="/product-search" class="position-relative newFormSarchBar" method="GET">
                     <select class="form-select selectSearchHome" aria-label="search home page" name="search_type" id="course_type">
                         <?php
                         foreach($type_course as $type)
