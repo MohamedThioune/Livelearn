932d94bf5fca95b2b1515d9d34b0e05428fd7759
Ibrahima Faye <ibouddha1.if@gmail.com>
2024-01-17 10:42:13 +0000

    databank fixed

--- templates/template-plugin.php
+++ templates/template-plugin.php
@@ -77,6 +77,10 @@ if (isset($selectedValues)) {
             if ($article != null) {
                 $sql_title = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}databank where titel=%s and type=%s", array($article['title']['rendered'], 'Artikel'));
                 $result_title = $wpdb->get_results($sql_title);
+                if(!$article['content']['rendered']){
+                    
+                }else
+                    var_dump('No content');
                 
                 // var_dump($article['excerpt']['rendered']);
                 // die;
