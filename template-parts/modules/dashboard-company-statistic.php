5da868bf8e88311f30f42a36c80e735aec8c04a2
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-03-27 03:04:42 +0100

    Live commit

--- template-parts/modules/dashboard-company-statistic.php
+++ template-parts/modules/dashboard-company-statistic.php
@@ -345,7 +345,8 @@ if(in_array('administrator', $current_user->roles) || in_array('hr', $current_us
                         </select> -->
                     </div>
                     <div>
-                        <canvas id="ChartEngagement"></canvas>
+                        <!-- <canvas id="ChartEngagement"></canvas> -->
+			     <span>No data enough !</span>
                     </div>
                 </div>
                 <div class="card-circular-bar">
