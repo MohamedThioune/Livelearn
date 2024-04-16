ec665a734af519078c55ce8799a4859cfb1fd546
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-04-16 12:11:35 +0000

    Script Fix --fillUp

--- script-endpoints.php
+++ script-endpoints.php
@@ -14,7 +14,7 @@ function fillUpCompany(){
     $courses = $wpdb->get_results($sql);
     // Make pagination
     $count = count($courses);
-    define("STEP", 3000);
+    define("STEP", 6000);
     $number_iteration = intval(ceil($count / STEP));
 
     echo  "<h1 class='textOpleidRight text-center alert alert-success'>the number of iteration are [ 1 to => $number_iteration ]</h1>";
