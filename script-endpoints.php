a13debdfafe8f7871ea5f7c7f63cff2a1ff0c531
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-04-16 12:52:44 +0000

    Script Fix --fillUp --language

--- script-endpoints.php
+++ script-endpoints.php
@@ -187,7 +187,7 @@ function updateLangaugeCourses()
     $courses = get_posts($args);
     //pagination
     $count = count($courses);
-    define("STEP", 500);
+    define("STEP", 200);
     $number_iteration = intval(ceil($count / STEP));
 
     echo  "<h1 class='textOpleidRight text-center alert alert-success'>the number of iteration are [ 1 to => $number_iteration ]</h1>";
