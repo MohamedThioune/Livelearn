a52c437748e528419a1b28c9ddcbcf40269ed516
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-04-16 14:00:37 +0000

    Script Fix --fillUp --language

--- script-endpoints.php
+++ script-endpoints.php
@@ -89,7 +89,7 @@ function fillUpAuthor(){
 
     // Make pagination
     $count = count($courses);
-    define("STEP", 1000);
+    define("STEP", 700);
     $number_iteration = intval(ceil($count / STEP));
 
     echo  "<h1 class='textOpleidRight text-center alert alert-success'>the number of iteration are [ 1 to => $number_iteration ]</h1>";
