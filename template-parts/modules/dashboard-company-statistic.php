423ec7b0cefae03e5c5ce7396f9935d16f8f6efd
Khadim Niass <niassssn@gmail.com>
2024-04-22 15:44:14 +0000

    statistic dynamicaly---

--- template-parts/modules/dashboard-company-statistic.php
+++ template-parts/modules/dashboard-company-statistic.php
@@ -357,6 +357,7 @@ if(in_array('administrator', $current_user->roles) || in_array('hr', $current_us
                     <div>
                         <canvas id="ChartCourse"></canvas>
                     </div>
+
                 </div>
                 <div class="card-circular-bar">
                     <div class="head d-flex justify-content-between align-items-center">
