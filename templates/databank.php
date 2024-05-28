b7c20a5ab02fc46a4c382aadcf96bbd215fd32fb
Fallou Ndiaye <fallou.ndiaye95@univ-thies.sn>
2024-05-28 11:50:28 +0200

    cron job databank

--- templates/databank.php
+++ templates/databank.php
@@ -1009,7 +1009,7 @@ if (!empty($courses)) {
             // Send selectedValues array via AJAX to PHP file
             $.ajax({
                 type: "POST",
-                url: "/livelearn/xml-parse",
+                url: "/xml-parse",
                 data: { selectedxmlValues: selectedxmlValues },
                 success: function(response) {
                     console.log(response);
