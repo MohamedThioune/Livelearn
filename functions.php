2d903f9e77469b1e32cd16a7b2f87b1bf8710185
Khadim Niass <niassssn@gmail.com>
2024-05-31 12:46:34 +0000

    make new endpoint for all experts

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ include "podcast-endpoints.php";
 include "video-endpoints.php";
 include "liggeey-endpoints.php";
 include "script-endpoints.php";
+include "dashbord-endpoints.php";
 
 require __DIR__ . '/templates/recommendation-module.php';
 require __DIR__ . '/templates/search-module.php';
@@ -2044,4 +2045,8 @@ add_action( 'rest_api_init', function () {
     'callback' => 'stripe'
   ));
 
+    register_rest_route ('custom/v1', '/tofollow/experts', array(
+        'methods' => 'GET',
+        'callback' => 'expertsToFollow'
+    ));
 });
