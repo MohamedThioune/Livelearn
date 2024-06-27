69f381ae0538ed832dbe058b79d2bbe6d07c474d
pro <pro@mbp-de-pro.home>
2024-06-27 12:17:02 +0000

    modal payement

--- templates/template-expert-ranking.php
+++ templates/template-expert-ranking.php
@@ -496,10 +496,6 @@ foreach($categories as $categ){
                     </div>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
         </div>
     </div>
 </div>
