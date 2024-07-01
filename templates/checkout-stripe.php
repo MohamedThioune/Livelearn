ce0f9a7b539e396f342f14e2447d3af2c66296ae
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-07-01 16:33:03 +0000

    checkout --draft

--- templates/checkout-stripe.php
+++ templates/checkout-stripe.php
@@ -6,7 +6,7 @@
 //Price ID
 if(isset($_GET['priceID'])):
     $key_password = "C@##me1995.";
-    $price_id = openssl_decrypt($encryptedPriceID, "AES-128-ECB", $key_password);    
+    $price_id = openssl_decrypt($_GET['priceID'], "AES-128-ECB", $key_password);    
 endif;
 ?>
 <head>
@@ -21,7 +21,7 @@ endif;
     // Create a Checkout Session
     async function initialize() {
     const fetchClientSecret = async () => {
-        const response = await fetch("/checkout-module/?priceID="<?= $price_id ?> , {
+        const response = await fetch("/checkout-module/?priceID="<?php echo $price_id ?> , {
         method: "POST",
         });
         const { clientSecret } = await response.json();
@@ -146,7 +146,7 @@ endif;
            </div>
            <!-- </form> -->
         </div>
-        
+
         <br><br>
         <div class="contentFormSubscription">
             <div id='stripe-checkout-first'>
