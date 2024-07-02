52a5bdfd746d08b6f47af4a4037330f3491c1dbc
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-07-02 11:17:46 +0000

    checkout --first step on somethin

--- templates/checkout.php
+++ templates/checkout.php
@@ -81,6 +81,6 @@ function session_stripe($price_id, $mode){
 
 //Call stripe secret
 // if(isset($_GET['priceID']) && $_GET['mode']):
-    $session_stripe_secret = session_stripe("price_1PY4QLEuOtOzwPYXkYQacx3q", "mode");
+    $session_stripe_secret = session_stripe("price_1PY4QLEuOtOzwPYXkYQacx3q", "payment");
     echo($session_stripe_secret);
 // endif;
