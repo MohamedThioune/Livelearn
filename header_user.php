4873292de9f0d3929583e74b55523242a2b6b2b8
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-07-09 14:47:47 +0000

    Checkout stripe --header-user ++conflict

--- header_user.php
+++ header_user.php
@@ -99,15 +99,6 @@ $see_experts = get_users(
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Google Tag Manager
-    <script>
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5NTJ5Z4');
-    </script>
-    End Google Tag Manager -->
     <meta name="description" content="Fluidify">
     <meta name='keywords' content="fluidify">
     <meta charset="utf-8">
@@ -156,7 +147,6 @@ $see_experts = get_users(
             display: none;
         }
     </style>
-
 
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
