a1a79c33daede5d4a25d6c238b371ce367919c79
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-09-26 19:38:02 +0000

    Endpoint post detail --more informations

--- liggeey-endpoints.php
+++ liggeey-endpoints.php
@@ -870,10 +870,13 @@ function postDetail(WP_REST_Request $request){
 
   $artikel = get_page_by_path($param_post_id, OBJECT, 'post');
   $sample = artikel($artikel->ID);
+  var_dump($sample);
+  echo '-----------';
 
   if(!empty($sample)):
     //Get further information
     $sample = postAdditionnal($sample);
+    var_dump($sample);
   endif;
 
   //Response
