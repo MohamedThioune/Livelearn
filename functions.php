45c0bfb88fefbef9d05da0f15b09e235e69664b0
Mohamed Thioune <mokhouthioune96@gmail.com>
2024-11-18 20:01:39 +0000

    Stripe payment redirection

--- functions.php
+++ functions.php
@@ -23,8 +23,8 @@ function create_session_stripe($data){
     return $information;
 }
 function session_stripe($price_id, $mode, $post_id = null, $user_id = null, $offline = null, $ui_mode = null){
-    $SITE_URL = "https://app.livelearn.nl/";
-    $YOUR_DOMAIN = (!$user_id || $user_id == 'null') ?  get_site_url() . '/inloggen' : get_site_url() . '/dashboard/user/activity';
+    $SITE_URL = "https://app.livelearn.nl";
+    $YOUR_DOMAIN = (!$user_id || $user_id == 'null') ?  get_site_url() . '/login' : get_site_url() . '/user/my-activities';
     $PRICE_ID = ($price_id) ?: null;
     $offline = ($offline) ?: null;
     $ui_mode = ($ui_mode) ? 'hosted': 'embedded';
