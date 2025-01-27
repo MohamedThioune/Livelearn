aa483f6f4c19b358cb5cebe51b0002798fc97220
Mohamed Thioune <mokhouthioune96@gmail.com>
2025-01-27 19:02:53 +0000

    Category overview + product search -test

--- dashbord-endpoints.php
+++ dashbord-endpoints.php
@@ -4056,6 +4056,9 @@ function all_courses_in_plateform_test()
     $courses = get_posts($args);
     $all_courses = array();
 
+    var_dump($courses);
+    return 0;
+    
     foreach ($courses as $course) {
         $course->visibility = get_field('visibility',$course->ID) ?? [];
         if ($course -> post_author) {
