2603f4f1a5108a12569260a1d300b78281aa9792
Cheikh Mouhamadou Fadhilou GUEYE <62220432+fadel221@users.noreply.github.com>
2025-01-29 13:29:53 +0000

    update notification function

--- functions.php
+++ functions.php
@@ -2852,7 +2852,7 @@ add_action( 'rest_api_init', function () {
 
     register_rest_route ('custom/v1', 'push/notifications/send', array(
         'methods' => 'POST',
-        'callback' => 'sendPushNotificationFirebase'
+        'callback' => 'sendNotificationToUser'
     ));
 
     /**
